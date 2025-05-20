63994fdc435133c3cc86d8271dcd0d3a6e2a07fc
amugunahe <wwaq1@naver.com>
2025-05-20 15:19:51 +0900

    20250520 Test

---
+++ 20250520/Test/Test02.java
@@ -0,0 +1,24 @@
+package com.ioex;
+
+/*문제 2) 문자 하나를 입력 받아 문자가 산술 연산자인지 아닌지 
+ *       산술연산자: ('+ , - , *, /, %)
+		 단, 모든 조건을 삼항연산자로 처리하시오.
+*/
+import java.io.*;
+
+public class Test02 {
+	
+	public static void main(String[] args) throws IOException {
+		char yon;
+		boolean bool = false;
+		
+		System.out.print("산술 연산자 입력(+, -, *, /, %):");
+		yon=(char)System.in.read();
+		
+		bool= yon == '+'|| yon == '-' || yon =='*' || yon== '/'
+			|| yon == '%'? true:false; 	
+		System.out.print(bool+"는 산술연산자"+ (bool? "입": "가 아닙")+"니다");
+
+	}
+}
+
