a08db74c6c91688019ef7f8f6fa92bb201051d38
amugunahe <wwaq1@naver.com>
2025-05-20 17:02:44 +0900

    study

--- 20250520- 테스트,if/if문/IfEx02_02.java
+++ 20250520- 테스트,if/if문/IfEx02_02.java
@@ -1,14 +1,12 @@
 package com.condition;
 
-/*문]
- * 사용자로부터 임의의 정수를 입력받아 
- * 입력받은 정수가 짝수인지 홀수인지를 판정하는 프로그램
- * 
+
+ /* 
  * 삼항 연산자를 이용하여 계산하셈
  *
  */
 import java.io.*;
-public class IfEx02_1 {
+public class IfEx02_02 {
 
 	public static void main(String[] args) throws IOException {
 		// BufferedReader 객체 생성
@@ -18,20 +16,10 @@ public class IfEx02_1 {
 		int n;  //사용자로부터 입력받은 정수를 저장할 변수
 		String str;  //홀수인지 짝수인지 대한 판결에 대한 결과를 저장하는 변수
 		
-		/* 연산 및 처리 
-		 * 1. 안내 메시지 출력 */
-		System.out.print("정수 입력:");
+		System.out.print("정수 입력: ");
 		n= Integer.parseInt(br.readLine());
 		
-		
-		//홀수인지 짝수인지를 판정
-		if(n%2 == 0) {
-			str= "짝수"; 
-		}else {
-			str= "홀수";
-		}
-		System.out.println(n+ "==> "+ str);
-		
+		str= n % 2 == 0? "짝수": "홀수";
+		System.out.println(n + "==> " + str);
 	}
-
-}
+	}
