965351a66f3a95c510e533aae716b71c61219748
amugunahe <wwaq1@naver.com>
2025-05-23 09:02:57 +0900

    study

---
+++ 20250521- Switch/IfEx02_02.java
@@ -0,0 +1,25 @@
+package com.condition;
+
+
+ /* 
+ * 삼항 연산자를 이용하여 계산하셈
+ *
+ */
+import java.io.*;
+public class IfEx02_02 {
+
+	public static void main(String[] args) throws IOException {
+		// BufferedReader 객체 생성
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		//주요 변수를 선언
+		int n;  //사용자로부터 입력받은 정수를 저장할 변수
+		String str;  //홀수인지 짝수인지 대한 판결에 대한 결과를 저장하는 변수
+		
+		System.out.print("정수 입력: ");
+		n= Integer.parseInt(br.readLine());
+		
+		str= n % 2 == 0? "짝수": "홀수";
+		System.out.println(n + "==> " + str);
+	}
+	}
---
+++ 20250521- Switch/IfEx07.java
@@ -0,0 +1,34 @@
+package com.condition;
+
+import java.io.*;
+public class IfEx07 {
+
+	public static void main(String[] args)throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int a, b; 
+		char c;
+		
+		System.out.print("첫번째 정수 입력: ");
+        a= Integer.parseInt(br.readLine());
+		
+		System.out.print("연산자 입력:");
+		c= (char)System.in.read();
+		System.in.read();
+		System.in.read();
+		System.in.skip(2);
+		
+		System.out.println("두번째 정수 입력: ");
+	    b= Integer.parseInt(br.readLine());
+		
+		
+		if(c== '+') {
+			System.out.printf("%d +%d= %d",a,b, (a+b));
+		} else if(c == '-') {
+			System.out.printf("%d - %d= %d", a, b, (a=b));
+		} else if(c == '*') {
+			System.out.printf("%d * %d= %d", a, b, (a*b));
+		} else if(c == '/') {
+			System.out.printf("%d / %d= %d", a, b, (a/b));
+		}
+	}
+}
