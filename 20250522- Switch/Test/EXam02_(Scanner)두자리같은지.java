40ed4dc8f072ff6359a0b7be29c94c991833c8bc
amugunahe <wwaq1@naver.com>
2025-05-23 17:41:38 +0900

    study

---
+++ 20250522- Switch/Test/EXam02_(Scanner)두자리같은지.java
@@ -0,0 +1,63 @@
+package com.exam;
+
+/*
+ * 문] 2자리의 정수(0~99)사이의 정수를 입력받고,
+ * 10의 자리와 1의 자리가 같은지를 판별하는 프로그램을 구현하시오.
+ * 
+ * 출력
+ * 2자리 정수 입력(10~99): 77
+ * 10의 자리와 1의 자리가 같습니다.  
+ * 
+ * 2자리 정수 입력(10~99): 57
+ * 10의 자리와 1의 자리가 다릅니다.  
+ * 
+ * 10의자리와 1의 자리를 구분 
+ * 몫은 10의자리로 나머지는 1의 자리로 
+ */
+
+import java.util.*;
+public class EXam02 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a;
+		
+		System.out.print("10자리 정수를 입력해라: ");
+		a= sc.nextInt();
+		
+		if(a > 9 && a<=99) {
+			if(a/10 == a%10)
+				System.out.println("10자리와 1의 자리가 같습니다.");
+			else
+				System.out.println("10의 자리와 1의 자리가 다릅니다.");
+		}	
+		else
+			System.out.println("범위를 벗어났습니다.");			
+		}
+	}
+
+/*
+
+ *  다른 답) 
+ * 		Scanner sc = new Scanner(System.in);
+		int n, ten, one;
+		
+		System.out.print("2자리 정수를 입력(10~99): ");
+		n= sc.nextInt();
+		
+		//범위
+		if(n<10 || n>99){
+		System.out.Pritnln("정수의 범위는 10~99만 입력하세요.");
+		return;
+		}
+		
+		ten = n/10;   //10의 자리
+		one = n%10;  // 1의 자리
+	
+	//십의 자리와 일의 자리가 같은지를 비교
+	  if(ten== one)
+	  System.out.Pritnln("10자리와 1의 자리가 같습니다.");
+		else
+	  System.out.Pritnln("10의 자리와 1의 자리가 다릅니다.");
+ */
+
---
+++ 20250522- Switch/Test/Exam03_(Scanner)지폐,동전.java
@@ -0,0 +1,94 @@
+package com.exam;
+
+/*
+ * 문]
+ *  정수로 된 돈의 금액을 입력받아 오만원권, 만원권, 오천원권, 천원권, 
+ *  500원 동전 100원 동전, 50원 동전, 10원 동전, 1원 동전이
+ *  각각 몇개인지를 반환하는 프로그램을 구현하시오.
+ *  
+ *  단, if문만 사용하여 구현.
+ *  
+ *  65376
+ *  오만원권 1매
+ *  만원권 1매
+ *  천원권 5매
+ *  100원 3개
+ *  50원 1개
+ *  10원 2개
+ *  1원  6개 
+ *
+ * 
+ */
+
+import java.util.*;
+
+public class Exam03 {
+	final static int omanwon= 50000;
+	final static int manwon= 10000;
+	final static int ochunwon= 5000;
+	final static int chunwon= 1000;
+	final static int obekwon= 500;
+	final static int bekwon= 100;
+	final static int osipwon=50;
+	final static int sipwon= 10;
+	final static int ilwon= 1;
+	
+
+	public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	
+	//돈의 종류 상수화 시킴
+		int money, result;
+		
+		System.out.print("금액을 입력하세요: ");
+		money= sc.nextInt();
+		
+		result = money/omanwon; //오만원권 개수
+		money = money % omanwon;  //돈이 수정
+		if(result > 0)
+			System.out.println("오만원권: "+ result + "매 입니다.");
+		
+		result = money/manwon; //만원권 개수
+		money = money % manwon;  //돈이 수정
+		if(result > 0)
+			System.out.println("만원권: "+ result + "매 입니다.");
+		
+		result = money/ochunwon; //오천원권 개수
+		money = money % ochunwon;  //돈이 수정
+		if(result > 0)
+			System.out.println("오천원권: "+ result + "매 입니다.");
+		
+		result = money/chunwon; //천원권 개수
+		money = money % chunwon;  //돈이 수정
+		if(result > 0)
+			System.out.println("천원권: "+ result + "매 입니다.");
+	
+		result = money/obekwon; //500원 개수
+		money = money % obekwon;  //돈이 수정
+		if(result > 0)
+			System.out.println("500원: "+ result + "개 입니다.");
+		
+		result = money/bekwon; //100원 개수
+		money = money % bekwon;  //돈이 수정
+		if(result > 0)
+			System.out.println("100원: "+ result + "개 입니다.");
+		
+		result = money/osipwon; //50원 개수
+		money = money % osipwon;  //돈이 수정
+		if(result > 0)
+			System.out.println("50원: "+ result + "개 입니다.");
+		
+		result = money/sipwon; //10원 개수
+		money = money % sipwon;  //돈이 수정
+		if(result > 0)
+			System.out.println("10원: "+ result + "개 입니다.");
+		
+		result = money/ilwon; //1원 개수
+		money = money % ilwon;  //돈이 수정
+		if(result > 0)
+			System.out.println("1원: "+ result + "개 입니다.");
+	
+		
+	}
+
+}
