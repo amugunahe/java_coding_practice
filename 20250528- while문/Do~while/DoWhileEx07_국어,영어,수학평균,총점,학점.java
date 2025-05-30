4b572adea3e8735a8c880cfd9a6e512521feb46b
amugunahe <wwaq1@naver.com>
2025-05-30 16:22:24 +0900

    study

--- 20250528- while문/Do~while/DoWhileEx07_국어,영어,수학평균,총점,학점.java
+++ 20250528- while문/Do~while/DoWhileEx07_국어,영어,수학평균,총점,학점.java
@@ -14,27 +14,55 @@ public class DoWhileEx07 {
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
-		int kor, eng, mat, sum = 0, avg = 0;
+		int kor, eng, mat, sum = 0;
+		char c, hak;
+		double avg = 0;
 
 		do {
 			System.out.print("국어 점수: ");
 			kor = sc.nextInt();
+			if (kor > 100)
+			System.out.println("다시 입력하세요.");
 		} while (kor < 0 || kor > 100);
 
 		do {
 			System.out.print("영어 점수: ");
 			eng = sc.nextInt();
+			if (eng > 100)
+				System.out.println("다시 입력하세요.");
 		} while (eng < 0 || eng > 100);
 
 		do {
 			System.out.print("수학 점수: ");
 			mat = sc.nextInt();
-			sum= kor+eng+mat;
-			avg= sum/3;
+			if (mat > 100)
+				System.out.println("다시 입력하세요.");
 		} while (mat < 0 || mat > 100);
 
-		System.out.println("합계: "+ sum);
-		System.out.println("평균: "+ avg);
-	}
+		sum = kor + eng + mat;
+		avg = sum / 3.0;
+		
+		switch((int)(avg/10)){
+		case 10:
+		case 9:
+			hak='A';
+			break;
+		case 8:
+			hak='B';
+			break;
+		case 7:
+			hak='C';
+			break;
+		case 6:
+			hak='D';
+			break;
+		default:
+			hak='F';
+	
+		}
 
+		System.out.println("총점: " + sum);
+		System.out.printf("평균: %.3f\n", avg);
+		System.out.println("학점: "+ hak);
+	}
 }
---
+++ 20250530-for문,while문/ForEx02_임의 정수 입력받아 10단위 출력.java
@@ -0,0 +1,73 @@
+package com.exam;
+
+/*
+ * 문] 사용자로부터 임의의 정수를 입력받아 1부터 입력 받은 수까지의 
+ * 전체의 합계, 짝수의 합계, 홀수의 합계를 각각 출력하는 프로그램을 구현하시오.
+ * 단, 10단위 출력하시오.
+ *
+ *출력
+ *정수 입력: 59
+ *1~10까지의 합계: 
+ *1~10까지의 짝수의 합계:
+ *1~10까지의 홀수의 합계:
+ *
+ *1~20까지의 합계: 
+ *1~20까지의 짝수의 합계:
+ *1~20까지의 홀수의 합계:
+ *
+ *1~30까지의 합계: 
+ *1~30까지의 짝수의 합계:
+ *1~30까지의 홀수의 합계:
+ *
+ *
+ *1~40까지의 합계: 
+ *1~40까지의 짝수의 합계:
+ *1~40까지의 홀수의 합계:
+ *
+ *1~50까지의 합계: 
+ *1~50까지의 짝수의 합계:
+ *1~50까지의 홀수의 합계:
+ *
+ *1~59까지의 합계: 
+ *1~59까지의 짝수의 합계:
+ *1~59까지의 홀수의 합계:
+ */
+
+import java.util.*;
+
+public class ForEx02 {
+
+	public static void main(String[] args) {
+		int n;
+		int sum;
+		int even;
+		int odd;
+		sum = even = odd = 0;
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("정수 입력: ");
+		n = sc.nextInt();
+
+		for (int i = 1; i <= n; i++) {
+			sum += i; // 전체의 합
+			if (i % 2 == 0) {
+				even += i; // 짝수의 합
+			} else {
+				odd += i; // 홀수의 합
+			}
+			if(i% 10==0) {  //10단위
+				System.out.println("1~"+i+"까지의 전체의 합계: " + sum);
+				System.out.println("1~"+i+"까지의 짝수 합계: " + even);
+				System.out.println("1~"+i+"까지의 홀수 합계: " + odd);
+			
+			}else if(i==n) {  //전부
+				System.out.println("1~"+i+"까지의 전체의 합계: " + sum);
+				System.out.println("1~"+i+"까지의 짝수 합계: " + even);
+				System.out.println("1~"+i+"까지의 홀수 합계: " + odd);
+			}
+		}
+
+	}
+
+}
