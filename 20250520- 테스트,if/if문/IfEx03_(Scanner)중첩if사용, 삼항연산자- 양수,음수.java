b048587974aba1e5d9da10f2faaa34f633a3a9af
bobman <wwaq1@naver.com>
2025-06-01 23:49:18 +0900

    Update IfEx03_(Scanner)중첩if사용, 삼항연산자- 양수,음수.java

--- 20250520- 테스트,if/if문/IfEx03_(Scanner)중첩if사용, 삼항연산자- 양수,음수.java
+++ 20250520- 테스트,if/if문/IfEx03_(Scanner)중첩if사용, 삼항연산자- 양수,음수.java
@@ -33,14 +33,15 @@ public class IfEx03 {
 		}
 	}
      /* 삼항 연산자
-	 str= (n<0)? "음수":((n==0)? "0": "양수");*/
+	 str = (n < 0) ? "음수" : ((n == 0) ? "0" : "양수");
 	
-	/* if(n ==0)
-	   str = "영";
-	  else if(n<0)
-	   str= "음수";
-	  else 
-	   str= "양수";
-	   System.out.println(n +"==>" + str);
+	/* 	if (n == 0) {
+			str = "영";
+		} else if (n < 0) {
+			str = "음수";
+		} else {
+			str = "양수";
+		}
+		System.out.println(n + "==>" + str);
 	 */
 }
