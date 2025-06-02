5c0a048539bc763d0dbd10da9a29e188cd9f1cc7
bobman <wwaq1@naver.com>
2025-06-02 20:05:42 +0900

    Update OperEx02_산술연산자.java

--- 20250516- 연산자, 삼항조건/OperEx02_산술연산자.java
+++ 20250516- 연산자, 삼항조건/OperEx02_산술연산자.java
@@ -11,11 +11,11 @@ public class OperEx02 {
 
 	public static void main(String[] args) {
 	short a, b;
-	a= 10;
-	b= 10;
-	//a= b= 10;
+	a = 10;
+	b = 10;
+	//a = b = 10;
 	
-	int c=  a + b;  //같은 자료형이 더해지면 결과는 int (short랑 short랑 더했다고 short가 되는게 아님)
+	int c = a + b;  //같은 자료형이 더해지면 결과는 int (short랑 short랑 더했다고 short가 되는게 아님)
 	System.out.println("c의 값은:" + c);
 
 	}
