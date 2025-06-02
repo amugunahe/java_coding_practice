dfcf62425196ef766503b8c9e826a13c0ed456d4
bobman <wwaq1@naver.com>
2025-06-02 20:04:02 +0900

    Update VarEx_참조자료형.java

--- 20250515- 변수, 상수/VarEx_참조자료형.java
+++ 20250515- 변수, 상수/VarEx_참조자료형.java
@@ -32,15 +32,15 @@ public class VarEx {
 
 	public static void main(String[] args) {
 	AA a; //클래스
-	a= new AA();
-	a.a=10;
+	a = new AA();
+	a.a = 10;
 	
-	int[]aa= new int[3]; //배열
-	aa[0]=10;
-	aa[1]=10;
-	aa[2]=10;
+	int[]aa = new int[3]; //배열
+	aa[0] = 10;
+	aa[1] = 10;
+	aa[2] = 10;
 	
-	for(int i=0; i< aa.length; i++) {
+	for(int i = 0; i < aa.length; i++) {
 		System.out.println(aa[i]);
 	}
 
