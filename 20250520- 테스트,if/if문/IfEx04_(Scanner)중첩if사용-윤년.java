a64a236b6f26431d2126a376769efbc45ef58dc1
bobman <wwaq1@naver.com>
2025-06-02 18:47:23 +0900

    Update IfEx04_(Scanner)중첩if사용-윤년.java

--- 20250520- 테스트,if/if문/IfEx04_(Scanner)중첩if사용-윤년.java
+++ 20250520- 테스트,if/if문/IfEx04_(Scanner)중첩if사용-윤년.java
@@ -31,7 +31,7 @@ public class IfEx04 {
 		  System.out.println(year + "은 평년 입니다."); }
 		 */
 		
-	if(year%4 == 0 && year%100 != 0) {
+	if(year % 4 == 0 && year % 100 != 0) {
 		System.out.println(year + "는 윤년입니다.");
 	}else {
 		if(year % 400 == 0) {
