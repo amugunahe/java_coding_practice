c394a3747ae4e3a58d033b70755db1dedc50f60a
bobman <wwaq1@naver.com>
2025-06-04 18:21:38 +0900

    Update Exam03_(Scanner)If지폐,동전.java

--- 20250522- Switch/Test/Exam03_(Scanner)If지폐,동전.java
+++ 20250522- Switch/Test/Exam03_(Scanner)If지폐,동전.java
@@ -23,15 +23,15 @@ package com.exam;
 import java.util.*;
 
 public class Exam03 {
-	final static int omanwon= 50000;
-	final static int manwon= 10000;
-	final static int ochunwon= 5000;
-	final static int chunwon= 1000;
-	final static int obekwon= 500;
-	final static int bekwon= 100;
-	final static int osipwon=50;
-	final static int sipwon= 10;
-	final static int ilwon= 1;
+	final static int omanwon = 50000;
+	final static int manwon = 10000;
+	final static int ochunwon = 5000;
+	final static int chunwon = 1000;
+	final static int obekwon = 500;
+	final static int bekwon = 100;
+	final static int osipwon = 50;
+	final static int sipwon = 10;
+	final static int ilwon = 1;
 	
 
 	public static void main(String[] args) {
@@ -41,52 +41,54 @@ public class Exam03 {
 		int money, result;
 		
 		System.out.print("금액을 입력하세요: ");
-		money= sc.nextInt();
+		money = sc.nextInt();
 		
-		result = money/omanwon; //오만원권 개수
+		result = money / omanwon; //오만원권 개수
 		money = money % omanwon;  //돈이 수정
 		if(result > 0)
-			System.out.println("오만원권: "+ result + "매 입니다.");
+			System.out.println("오만원권: " + result + "매 입니다.");
 		
-		result = money/manwon; //만원권 개수
+		result = money / manwon; //만원권 개수
 		money = money % manwon;  //돈이 수정
 		if(result > 0)
-			System.out.println("만원권: "+ result + "매 입니다.");
+			System.out.println("만원권: " + result + "매 입니다.");
 		
-		result = money/ochunwon; //오천원권 개수
+		result = money / ochunwon; //오천원권 개수
 		money = money % ochunwon;  //돈이 수정
 		if(result > 0)
-			System.out.println("오천원권: "+ result + "매 입니다.");
+			System.out.println("오천원권: " + result + "매 입니다.");
+
+
 		
-		result = money/chunwon; //천원권 개수
+		result = money / chunwon; //천원권 개수
 		money = money % chunwon;  //돈이 수정
 		if(result > 0)
-			System.out.println("천원권: "+ result + "매 입니다.");
+			System.out.println("천원권: " + result + "매 입니다.");
 	
-		result = money/obekwon; //500원 개수
+		result = money / obekwon; //500원 개수
 		money = money % obekwon;  //돈이 수정
 		if(result > 0)
-			System.out.println("500원: "+ result + "개 입니다.");
+			System.out.println("500원: " + result + "개 입니다.");
 		
 		result = money/bekwon; //100원 개수
 		money = money % bekwon;  //돈이 수정
 		if(result > 0)
-			System.out.println("100원: "+ result + "개 입니다.");
+			System.out.println("100원: " + result + "개 입니다.");
 		
-		result = money/osipwon; //50원 개수
+		result = money / osipwon; //50원 개수
 		money = money % osipwon;  //돈이 수정
 		if(result > 0)
-			System.out.println("50원: "+ result + "개 입니다.");
+			System.out.println("50원: " + result + "개 입니다.");
 		
-		result = money/sipwon; //10원 개수
+		result = money / sipwon; //10원 개수
 		money = money % sipwon;  //돈이 수정
 		if(result > 0)
-			System.out.println("10원: "+ result + "개 입니다.");
+			System.out.println("10원: " + result + "개 입니다.");
 		
-		result = money/ilwon; //1원 개수
+		result = money / ilwon; //1원 개수
 		money = money % ilwon;  //돈이 수정
 		if(result > 0)
-			System.out.println("1원: "+ result + "개 입니다.");
+			System.out.println("1원: " + result + "개 입니다.");
 	
 		
 	}
