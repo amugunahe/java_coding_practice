6d3b53774685bf1918533b1af8c6a9325d3e1cd5
bobman <wwaq1@naver.com>
2025-06-04 18:19:09 +0900

    Update Exam01_(Scanner)원화달러바꾸기.java

--- 20250522- Switch/Test/Exam01_(Scanner)원화달러바꾸기.java
+++ 20250522- Switch/Test/Exam01_(Scanner)원화달러바꾸기.java
@@ -18,30 +18,30 @@ public class Exam01 {
 		Scanner sc = new Scanner(System.in);
 		int won;   //원화를 입력받을 변수
 		
-		final double rate = 1400.0;  // 1$=1400
+		final double rate = 1400.0;  // 1$ = 1400
 		double dollar = 0;  //달러로 변환하여 저장할 변수
 		
 		System.out.print("원화 입력(숫자만 기입): ");
 		won = sc.nextInt();
 		
-		System.out.printf("%d원은 $ %.2f 입니다", won, (float)won/rate);	
+		System.out.printf("%d원은 $ %.2f 입니다", won, (float)won / rate);	
 		
 	}
 
 }
 
-/*Scanner sc = new Scanner(System.in);
+		/*Scanner sc = new Scanner(System.in);
 		int won;   //원화를 입력받을 변수
 		
-		final double rate = 1400.0;  // 1$=1400
+		final double rate = 1400.0;  // 1$ = 1400
 		double dollar = 0;  //달러로 변환하여 저장할 변수
 		
 		System.out.print("원화 입력(숫자만 기입): ");
 		won = sc.nextInt();
 		
-		dollar= won/rate;
+		dollar = won / rate;
 		
-		System.out.prinln(won+ "은 $"+ dollar+ "입니다");	
+		System.out.prinln(won + "은 $" + dollar + "입니다");	
 		
  * 
  */
