43047fed8b03a5b6a05b5be6242858eda7dafc26
bobman <wwaq1@naver.com>
2025-06-04 19:28:44 +0900

    Update SwitchEx05_02_con(Scanner)가위바위보if.java

--- 20250522- Switch/switch/SwitchEx05_02_con(Scanner)가위바위보if.java
+++ 20250522- Switch/switch/SwitchEx05_02_con(Scanner)가위바위보if.java
@@ -6,21 +6,20 @@ import java.util.Scanner;
 public class SwitchEx05_02_con {
 
 	public static void main(String[] args) {
-		Scanner sc= new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 		
 		System.out.print("가위(1), 바위(2), 보(3) 중 입력: ");
-		int user= sc.nextInt();
-		int com = (int)(Math.random()*3)+1;
+		int user = sc.nextInt();
+		int com = (int)(Math.random() * 3) + 1;
 		
 		System.out.println(user);
 		System.out.println(com);
 
-		if (user-com==2 || user-com==-1) { 
+		if (user - com == 2 || user - com == -1) { 
 		System.out.println("컴퓨터가 이겼습니다 ");
-		}
-		else if (user-com== 1 || user-com==2) {
-		System.out.println("사용자가 이겼습니다.");}
-		else if (user-com==0) {
+		}else if (user - com == 1 || user - com == 2) {
+		System.out.println("사용자가 이겼습니다.");
+		}else if (user - com == 0) {
 		System.out.println("비겼습니다.");}
 	}
 }
