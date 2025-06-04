85e3e3387357cdf678db217ca94589f31ad70c88
bobman <wwaq1@naver.com>
2025-06-04 19:33:46 +0900

    Update SwitchEx07(Scanner)커피주문.java

--- 20250522- Switch/switch/SwitchEx07(Scanner)커피주문.java
+++ 20250522- Switch/switch/SwitchEx07(Scanner)커피주문.java
@@ -17,12 +17,12 @@ import java.util.*;
 public class SwitchEx07 {
 
 	public static void main(String[] args) {
-		Scanner sc= new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 		
 		String str;
 		System.out.print("커피 종류 입력(에스프레소, 카푸치노, 카페라떼, 아메리카노): ");
-		str= sc.next();
-		System.out.println("무슨 커피: "+ str);
+		str = sc.next();
+		System.out.println("무슨 커피: " + str);
 		
  
 
@@ -35,8 +35,8 @@ public class SwitchEx07 {
 				break;
 		default:
 			System.out.println("메뉴에 없습니다.");
+		}
 	}
-}
 }
 
 
@@ -45,11 +45,11 @@ public class SwitchEx07 {
  * String order;
  * int price = 0;
  * System.out.print("무슨 커피: ");
- * order= sc.next();
+ * order = sc.next();
  * 
  * switch(order){
  * case "에스프레소": case "카푸치노": case "카페라떼":
- * price= 3500;
+ * price = 3500;
  * break;
  * 
  * case "아메리카노":
@@ -60,6 +60,6 @@ public class SwitchEx07 {
 }
 
  * if(price != 0)
- * System.out.println(order+ " 는 " + price+ "입니다.");
+ * System.out.println(order + " 는 " + price + "입니다.");
  *
  */
