37de5570a778fe8c5696469d77423de46d0bc7e2
bobman <wwaq1@naver.com>
2025-06-04 20:27:53 +0900

    Update ForEx02_02_1~100까지총합,홀,짝합계.java

--- 20250526- for문/ForEx02_02_1~100까지총합,홀,짝합계.java
+++ 20250526- for문/ForEx02_02_1~100까지총합,홀,짝합계.java
@@ -15,9 +15,9 @@ public class ForEx02_02 {
 		for (i = 1; i <= 100; i++) {
 
 			if (i % 2 == 0)
-				even += i;  //홀수의 합
+				even += i;  //짝수의 합
 			else
-				odd += i;  //짝수의 합 
+				odd += i;  //홀수의 합 
 
 			sum += i; // 전체 합계
 		}
