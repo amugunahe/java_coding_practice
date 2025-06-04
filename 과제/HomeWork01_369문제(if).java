12e6b4e7944bff1d69a77ca26a88dc491a963668
bobman <wwaq1@naver.com>
2025-06-04 18:10:27 +0900

    Update HomeWork01_369문제(if).java

--- 과제/HomeWork01_369문제(if).java
+++ 과제/HomeWork01_369문제(if).java
@@ -15,77 +15,76 @@ public class HomeWork01 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		int num= 0;
+		int num = 0;
 		
 		System.out.print("1~99 사이의 정수 입력: ");
 		num = sc.nextInt();
 		
 		
-		if(num>0 && num<100) {
+		if(num > 0 && num < 100) {
 	}
 		else {
 			System.out.println("범위보고 다시 입력하세요.");
 			return;
 		}
 		
-		int chak = num/10;
-		int chakchak= num%10;
+		int chak = num / 10;
+		int chakchak = num % 10;
 		
 	
-			if (chak==0||chak==1|| chak==2|| chak==4||chak==5||chak==7||chak==8) {
-			 	if(chakchak==3 || chakchak==6 || chakchak==9) {
+			if (chak == 0||chak == 1|| chak == 2|| chak == 4||chak == 5||chak == 7||chak == 8) {
+			 	if(chakchak == 3 || chakchak == 6 || chakchak == 9) {
 			 		System.out.println("박수 짝");
 			 	}
 			 	else{
 			 		System.out.println("박수 X");{
 			 	}
 			 	}
-			} else if((chak==3||chak==6||chak==9)&&(chakchak==3)||(chakchak==6)||(chakchak==9))  {
+			} else if((chak == 3||chak == 6||chak == 9)&&(chakchak == 3)||(chakchak == 6)||(chakchak == 9))  {
 				System.out.println("박수 짝짝");
-			} else if (chak==3||chak==6||chak==9) {
+			} else if (chak == 3||chak == 6||chak == 9) {
 				System.out.println("박수 짝");
-			} else if (chakchak==3||chakchak==6||chakchak==9)
+			} else if (chakchak == 3||chakchak == 6||chakchak == 9)
 				System.out.println("박수 짝");
 				}		
 
 	}
 /*
   답안]
- 	int n, cnt=0;
+ 	int n, cnt = 0;
  	int a,b;  //a는 10의자리 b는 1의자리 
  	
   	Scanner sc = new Scanner(System.in);
- 		System.out.printt("1~99 사이의 정수: ");
-		n=sc.nextInt();
+ 		System.out.print("1~99 사이의 정수: ");
+		n = sc.nextInt();
 		
  	
  	//1~99까지의 정수 범위
- 	if(n>=1&& n<=99){
+ 	if(n >= 1 && n <= 99){
  
- 	a= n/10;   //10의 자리 
- 	b= n%10;  // 1의 자리 
+ 	a = n / 10;   //10의 자리 
+ 	b = n % 10;  // 1의 자리 
  
  //십의 자리 정수에 3,6,9 중 하나가 있는 경우 cnt를 증가시킴
-  	if(a !=0 && a %3 ==0){
+  	if(a != 0 && a % 3 == 0){
   	cnt++;
   }
  //일의 자리 정수에 3,6,9 중 하나가 있는 경우 cnt를 증가시킴 
- 	if (b !=0 && b%3==0){
+ 	if (b != 0 && b % 3 == 0){
  	cnt++;
  	}
  	
- 	if (cnt ==2) 
+ 	if (cnt == 2) 
  		System.out.println("박수 짝짝");
- 	else if (cnt ==1)
+ 	else if (cnt == 1)
  		System.out.println("박수 짝");
- 	else if (cnt ==0)
+ 	else if (cnt == 0)
  		System.out.println("박수 없음");
   	}else {
   	System.out.println("숫자의 범위를 벗어났습니다.");
   	return;
   	}
  }
-  
   
  */
 
