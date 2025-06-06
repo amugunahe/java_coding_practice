6d782314e57fc866e1b388028aed202cf15e541f
bobman <wwaq1@naver.com>
2025-06-07 01:31:13 +0900

    Update Test01_숫자받아남성여성글자출력.java

--- 20250520- 테스트,if/Test/Test01_숫자받아남성여성글자출력.java
+++ 20250520- 테스트,if/Test/Test01_숫자받아남성여성글자출력.java
@@ -1,12 +1,13 @@
 package com.ioex;
 
-
-/*문제1) 숫자 하나를 입력받아 그 수가 1이면 "남성, 2이면 "여성"이라는 글자를 출력하는 프로그램, "
-		"단 모든 조건을 삼항연산자로 처리하시오
+/*문제1) 숫자 하나를 입력받아 그 수가 1이면 "남성, 2이면 "여성"이라는 글자를 출력하는 프로그램, 
+	 단 모든 조건을 삼항연산자로 처리하시오
 */
+
 import java.io.*;
 
 public class Test01 {
+	
 	public static void main(String[] args) throws IOException {
 	
 		int gender = 0;
@@ -15,7 +16,6 @@ public class Test01 {
 		gender = System.in.read()-48;
 		
 		String str = (gender == 1? "남성" : "여성");
-		//System.out.println("당신은 " + (gender == 1? "남성" : "여성") + "입니다.");
-		System.out.println("당신은 " + str + "입니다.");
-}
+		System.out.println("당신은 " + str + "입니다.");  //System.out.println("당신은 " + (gender == 1? "남성" : "여성") + "입니다.");
+	}
 }
