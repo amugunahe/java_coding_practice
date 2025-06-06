d06b6270a5bf4dcccdc3ece5ba1a90023f6e51b4
bobman <wwaq1@naver.com>
2025-06-07 02:06:29 +0900

    Update ForEx06_알파벳 출력.java

--- 20250526- for문/ForEx06_알파벳 출력.java
+++ 20250526- for문/ForEx06_알파벳 출력.java
@@ -3,15 +3,18 @@ package com.loop;
 public class ForEx06 {
 
 	public static void main(String[] args) {
+		
 		for (char ch = 'A'; ch <= 'Z'; ch++) {
 			System.out.print(ch + " ");
 		}
 		System.out.println();
 		System.out.println();
+		
 		for (int ch = 65; ch <= 90; ch++) {
 			System.out.print((char) ch + " ");
 		}
 		System.out.println();
+		
 		for(char ch = 'A'; ch <='Z'; ch++) {
 			for(char bb = ch; bb <= 'Z'; bb++) {
 				System.out.print(bb);
