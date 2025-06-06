676954cda0651488642a00c3bace061d5de1cb13
bobman <wwaq1@naver.com>
2025-06-07 01:33:49 +0900

    Update InputEx02_(IOException)문자입력.java

--- 20250519- 입력,출력, scanner/InputEx02_(IOException)문자입력.java
+++ 20250519- 입력,출력, scanner/InputEx02_(IOException)문자입력.java
@@ -10,10 +10,10 @@ public class InputEx02 {
 	char ch = (char)System.in.read();
 		
 	/*
-	 * char ch = (char)*문자 출력을 위한 char 활용 System.in.read();로 입력 받은 값을 
-	 * 정수 값 그대로 활용하기 위함
-	 * [* System.in.read();는 아스키 코드 출력 관련] 문자를 출력시킬 때 오류가 발생했을 경우 throws IOException{}을 활용하여 
-	 * jAVA 버츄얼 머신에 오류 교정을 맡긴다
+	 char ch = (char)*문자 출력을 위한 char 활용 System.in.read();로 입력 받은 값을 
+	 정수 값 그대로 활용하기 위함
+	 [* System.in.read();는 아스키 코드 출력 관련] 문자를 출력시킬 때 오류가 발생했을 경우 throws IOException{}을 활용하여 
+	 jAVA 버츄얼 머신에 오류 교정을 맡긴다
 	 */
 		
 	System.out.println(ch);
