7be3cf5445e1694fb8bb9dcb108f1f6363f8e847
bobman <wwaq1@naver.com>
2025-06-07 00:48:06 +0900

    Update OperEx06_복합대입연산자.java

--- 20250516- 연산자, 삼항조건/OperEx06_복합대입연산자.java
+++ 20250516- 연산자, 삼항조건/OperEx06_복합대입연산자.java
@@ -2,12 +2,12 @@ package com.operex;
 
 /*복합대입연산자, 복합 연산자, 배정연산자
  * +=, -=, ....  a= a+b => a+=b
- *  
  */
 
 
 public class OperEx06 {
 	public static void main(String[] args) {
+			
 			int a = 10;
 			int result = 0;
 			
@@ -25,6 +25,5 @@ public class OperEx06 {
 		
 			result %= a;
 			System.out.println("result: " + result);
-			
 	}
 }
