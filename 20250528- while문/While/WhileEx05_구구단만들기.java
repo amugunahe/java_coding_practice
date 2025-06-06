866b88f6e32a7cad615e770f6d3da174ca778f52
bobman <wwaq1@naver.com>
2025-06-07 00:19:51 +0900

    Update WhileEx05_구구단만들기.java

--- 20250528- while문/While/WhileEx05_구구단만들기.java
+++ 20250528- while문/While/WhileEx05_구구단만들기.java
@@ -6,10 +6,11 @@ public class WhileEx05 {
 
 	public static void main(String[] args) {
 		int i = 2, j;
+		
 		while (i <= 9) {
 			j = 1; // j를 while문 안쪽으로 넣어줘야함
 			while (j <= 9) {
-				System.out.printf("%d * %d= %d%n", i, j, i * j);
+				System.out.printf("%d * %d = %d%n", i, j, i * j);
 				j++;
 			}
 			i++;
