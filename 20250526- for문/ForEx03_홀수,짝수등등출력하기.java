2b3d4743c40bd4364b95c16687277190bc739664
bobman <wwaq1@naver.com>
2025-06-07 02:05:28 +0900

    Update ForEx03_홀수,짝수등등출력하기.java

--- 20250526- for문/ForEx03_홀수,짝수등등출력하기.java
+++ 20250526- for문/ForEx03_홀수,짝수등등출력하기.java
@@ -3,8 +3,7 @@ package com.loop;
 public class ForEx03 {
 
 	public static void main(String[] args) {
-		/*
-		int sum = 0;
+		/* int sum = 0;
 		
 		for (int i = 1, j = 10, k = 10; i <= 10; i++, j--, k--) {
 			System.out.println(i + " => " + j + " => " + k);
@@ -12,8 +11,8 @@ public class ForEx03 {
 		 */
 		System.out.println("i\t 2 * i\t 2 * i - 1\t i * i\t 11 - i\t i % 3\t i / 3");
 		System.out.println("---------------------------------------");
+		
 		for(int i = 1; i <= 10; i++)
-			System.out.printf("%d\t %d\t %d\t %d\t %d\t %d\t %d%n"
-					, i, 2 * i, 2 * i - 1, i * i, 11 - i, i % 3, i / 3);
+			System.out.printf("%d\t %d\t %d\t %d\t %d\t %d\t %d%n", i, 2 * i, 2 * i - 1, i * i, 11 - i, i % 3, i / 3);
 	}
 }
