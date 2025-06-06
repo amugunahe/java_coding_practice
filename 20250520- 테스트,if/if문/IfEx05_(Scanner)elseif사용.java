1cad117d7497e5e91dc5810905523eec4ebf5440
bobman <wwaq1@naver.com>
2025-06-07 01:26:55 +0900

    Update IfEx05_(Scanner)elseif사용.java

--- 20250520- 테스트,if/if문/IfEx05_(Scanner)elseif사용.java
+++ 20250520- 테스트,if/if문/IfEx05_(Scanner)elseif사용.java
@@ -1,18 +1,20 @@
 package com.condition;
-/*
- *문] 
- *사용자로부터 임의의 정수를 입력받아 입력받은 정수가 
- *2의 배수인지, 3의 배수인지, 2와 3의 배수인지, 2와 3의 배수가 아닌지를
- *판정하는 프로그램을 구현하시오. 
+/*문] 
+ 사용자로부터 임의의 정수를 입력받아 입력받은 정수가 
+ 2의 배수인지, 3의 배수인지, 2와 3의 배수인지, 2와 3의 배수가 아닌지를
+ 판정하는 프로그램을 구현하시오. 
  */
  
-
 import java.util.*;
+
 public class IfEx05 {
 
 	public static void main(String[] args) {
+		
 	Scanner sc = new Scanner(System.in);
+		
 		int a;  //임의의 정수를 저장할 변수
+		
 		System.out.print("정수 입력:");
 		a = sc.nextInt();
 		
@@ -20,7 +22,6 @@ public class IfEx05 {
 			System.out.println("2와 3의 배수입니다.");
 		 else if (a % 2 == 0) 
 			System.out.println("2의 배수입니다");
-			
 			else if (a % 3 == 0)
 				System.out.println("3의 배수입니다.");
 			else 
@@ -28,5 +29,3 @@ public class IfEx05 {
 		}
 	
 }
-
-
