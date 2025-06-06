fd31b89b63543ccde48c23bfaa6e46798649879d
bobman <wwaq1@naver.com>
2025-06-07 01:06:10 +0900

    Update Exam01_스캐너로정보출력.java

--- 20250519- 입력,출력, scanner/Exam01_스캐너로정보출력.java
+++ 20250519- 입력,출력, scanner/Exam01_스캐너로정보출력.java
@@ -1,21 +1,25 @@
 package com.ioex;
 
-import java.util.*;
-public class Exam01 {
 /* 문제 Scanner 클래스로 이름, 도시, 나이, 체중, 독신 여부를 입력받고 출력하는 프로그램을 구현하시오
  * 
  * 문자열: 이름, 도시
  * 정수: 나이
  * 실수: 체중
  * 논리(boolean): 독신여부
- * */
+*/
+
+import java.util.*;
+
+public class Exam01 {
+
 	public static void main(String[] args) {
+		
 		Scanner sc = new Scanner(System.in);
 		
 		String name, city;
 		int age;
 		double weight;
-		boolean issingle; //boolean single = false;
+		boolean issingle; // boolean single = false;
 		
 		System.out.print("이름: ");
 		name = sc.next();
@@ -34,10 +38,9 @@ public class Exam01 {
 		
 		System.out.println("이름은 " + name + ", 도시는 " + city + " , 나이는 " + age + ", 체중은 "
 		+ weight + ", 독신 여부는 " + issingle + " 입니다." );
-		/*
-		 * System.out.println("나의 이름은 " + name + "이고 \n 거주지는" + city+ "이며 \n 나이는 "age + "이고
-		 * \n" + weight + "kg이고 + "독신 여부는 " + isSingle + " 입니다.");
-		 */	
+		
+		/* System.out.println("나의 이름은 " + name + "이고 \n 거주지는" + city+ "이며 \n 나이는 "age + "이고
+		 \n" + weight + "kg이고 + "독신 여부는 " + isSingle + " 입니다.");
+		*/	
 	}
-
 }
