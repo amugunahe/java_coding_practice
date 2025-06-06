1b5c5d856c242129262498b9bed1d0c46996bb4d
bobman <wwaq1@naver.com>
2025-06-07 00:50:42 +0900

    Update operEx05_삼항조건연산자.java

--- 20250516- 연산자, 삼항조건/operEx05_삼항조건연산자.java
+++ 20250516- 연산자, 삼항조건/operEx05_삼항조건연산자.java
@@ -1,19 +1,17 @@
 package com.operex;
 
 /* 삼항 조건 연산자 -> 삼항연산자
- * 조건항 ? 항1(true): 항2(false)
+ * 조건항 ? 항1(true) : 항2(false)
 */
 public class OperEx05 {
 
 	public static void main(String[] args) {
 
 		int a = 20, b = 30, max;
+		
 		max = a > b ? ++a : ++b;
-
 		System.out.println("max: " + max);
 		System.out.println("a: " + a);
 		System.out.println("b: " + b);
-
 	}
-
 }
