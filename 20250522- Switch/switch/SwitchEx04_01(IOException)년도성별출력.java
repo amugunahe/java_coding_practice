48b2701fe4a20a65cd8b4f04428db41374c7a4a3
bobman <wwaq1@naver.com>
2025-06-07 01:56:32 +0900

    Update SwitchEx04_01(IOException)년도성별출력.java

--- 20250522- Switch/switch/SwitchEx04_01(IOException)년도성별출력.java
+++ 20250522- Switch/switch/SwitchEx04_01(IOException)년도성별출력.java
@@ -1,20 +1,18 @@
 package com.switchex;
 
-/*
-* 문] 주민등록번호 7번째 자리를 입력받아 년도와 성별을 출력하는 프로그램을 구현하시오.
-* switch~case문 활용
-* 
-* 90 -> 1800
-* 12 -> 1900
-* 34 -> 2000
-*
+/* 문] 주민등록번호 7번째 자리를 입력받아 년도와 성별을 출력하는 프로그램을 구현하시오.
+switch~case문 활용
+ 
+90 -> 1800
+12 -> 1900
+34 -> 2000
 */
 
 import java.io.*;
 
 public class SwitchEx04_01 {
 
-	public static void main(String[] args)throws IOException {
+	public static void main(String[] args) throws IOException {
 	
 	System.out.print("주민등록번호 7번째 자리 입력: ");
 	int x = System.in.read()-48;
@@ -36,11 +34,8 @@ public class SwitchEx04_01 {
 	System.out.println("잘못 입력하셨습니다.");
 	System.exit(-1); //return;
 	}
-	
 	gender = (x % 2 == 0)? "여성" : "남성";
 				
 	System.out.println("당신은 " + year + "년대생 " + gender + " 입니다.");
-
-	}
-		
+	}	
 }
