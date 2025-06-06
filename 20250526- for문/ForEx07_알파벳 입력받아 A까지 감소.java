c198abbb76d0a5457ee756281aa253324163ddce
bobman <wwaq1@naver.com>
2025-06-07 02:04:20 +0900

    Update ForEx07_알파벳 입력받아 A까지 감소.java

--- 20250526- for문/ForEx07_알파벳 입력받아 A까지 감소.java
+++ 20250526- for문/ForEx07_알파벳 입력받아 A까지 감소.java
@@ -1,24 +1,23 @@
 package com.loop;
 
-/*	문]
- *  	알파벳 한문자를 입력받아 
- *  	입력받은 문자부터 A까지 감소시키며 출력하는 프로그램을 구현하시오.
- *  
- *   알파벳 입력: e
- *   abcde
- *   abcd
- *   abc
- *   ab
- *   a
- */
+/* 문] 알파벳 한문자를 입력받아 입력받은 문자부터 A까지 감소시키며 출력하는 프로그램을 구현하시오.
+
+알파벳 입력: e
+abcde
+abcd
+abcab
+a
+*/
 
 import java.util.*;
 
 public class ForEx07 {
 
 	public static void main(String[] args) {
-		System.out.print("알파벳 입력: ");
+		
 		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("알파벳 입력: ");
 		char alph = sc.next().charAt(0);
 		
 		for(char ch = 'A'; ch <= alph; ch++) {
