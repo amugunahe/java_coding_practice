6e6ad38fbe1ef6c01ceb6a84716219b1ac619568
bobman <wwaq1@naver.com>
2025-06-07 01:55:42 +0900

    Update SwitchEx03_(Scanner)월에맞는계절.java

--- 20250522- Switch/switch/SwitchEx03_(Scanner)월에맞는계절.java
+++ 20250522- Switch/switch/SwitchEx03_(Scanner)월에맞는계절.java
@@ -1,14 +1,15 @@
 package com.switchex;
 
-/*
- * 문] 월을 입력 받아서 월에 해당하는 계절을 출력하는 프로그램을 구현하시오.
- * 단, switch ~case문 활용 
- */
+/* 문] 월을 입력 받아서 월에 해당하는 계절을 출력하는 프로그램을 구현하시오.
+ 단, switch ~case문 활용 
+*/
 
 import java.util.*;
+
 public class SwitchEx03 {
 
 	public static void main(String[] args) {
+		
 	Scanner sc = new Scanner(System.in);
 	
 	int month;
@@ -32,8 +33,6 @@ public class SwitchEx03 {
 	default:
 		System.out.println("입력하신 수는 계절에 속하지 않습니다");
 		break;
+		}
 	}
-	
-	}
-
 }
