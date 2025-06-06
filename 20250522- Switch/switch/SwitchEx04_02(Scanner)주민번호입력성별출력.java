009b4f164ae31090d002bb23328a8823a111f84d
bobman <wwaq1@naver.com>
2025-06-07 01:56:55 +0900

    Update SwitchEx04_02(Scanner)주민번호입력성별출력.java

--- 20250522- Switch/switch/SwitchEx04_02(Scanner)주민번호입력성별출력.java
+++ 20250522- Switch/switch/SwitchEx04_02(Scanner)주민번호입력성별출력.java
@@ -1,10 +1,13 @@
 package com.switchex;
 
 import java.util. *;
+
 public class SwitchEx04_02 {
 
 	public static void main(String[] args) {
+		
 		Scanner sc = new Scanner(System.in);
+		
 		 System.out.print("주민등록번호 전부 입력(123456-1234567): "); 
 		 String jumin = sc.nextLine();
 		 
