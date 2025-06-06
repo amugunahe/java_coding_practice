230fea30e431e5ea71265668280a3e8db8da444b
bobman <wwaq1@naver.com>
2025-06-07 01:32:16 +0900

    Update Test02_문자가산술연산자인지아닌지.java

--- 20250520- 테스트,if/Test/Test02_문자가산술연산자인지아닌지.java
+++ 20250520- 테스트,if/Test/Test02_문자가산술연산자인지아닌지.java
@@ -1,14 +1,16 @@
 package com.ioex;
 
 /*문제 2) 문자 하나를 입력 받아 문자가 산술 연산자인지 아닌지를 판단하는 프로그램을 만드세요
- *       산술연산자: ('+ , - , *, /, %)
-		 단, 모든 조건을 삼항연산자로 처리하시오.
+산술연산자: ('+ , - , *, /, %)
+단, 모든 조건을 삼항연산자로 처리하시오.
 */
+
 import java.io.*;
 
 public class Test02 {
 	
 	public static void main(String[] args) throws IOException {
+		
 		char yon;
 		boolean bool = false;
 		
@@ -19,12 +21,10 @@ public class Test02 {
 			|| yon == '%'? true : false; 	
 		
 		/* if(yon == '+' || yon == '-' || yon == '*' || yon == '/' || yon == '%')
-		 * bool true;
-		 * else
-		 * bool false; 
+		  bool true;
+		  else
+		  bool false; 
 		 */
 		System.out.print(bool + "는 산술연산자" + (bool? "입" : "가 아닙") + "니다");
-
 	}
 }
-
