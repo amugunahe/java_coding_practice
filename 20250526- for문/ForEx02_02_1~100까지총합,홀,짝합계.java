eab2f8a597092a0fff95cbfe793fedbf87cbd509
bobman <wwaq1@naver.com>
2025-06-07 02:04:50 +0900

    Update ForEx02_02_1~100까지총합,홀,짝합계.java

--- 20250526- for문/ForEx02_02_1~100까지총합,홀,짝합계.java
+++ 20250526- for문/ForEx02_02_1~100까지총합,홀,짝합계.java
@@ -1,19 +1,17 @@
 package com.loop;
 
-/*
- * 문] 1~100까지의 수중 전체 합계, 홀수의 합, 짝수의 합을 구하는 프로그램을 구현하시오.
- * 
+/* 문] 1~100까지의 수중 전체 합계, 홀수의 합, 짝수의 합을 구하는 프로그램을 구현하시오.
  */
 
 public class ForEx02_02 {
 
 	public static void main(String[] args) {
+		
 		int i, sum = 0;
 		int odd = 0;   // 홀수의합
 		int even = 0; // 짝수의 합
 
 		for (i = 1; i <= 100; i++) {
-
 			if (i % 2 == 0)
 				even += i;  //짝수의 합
 			else
@@ -25,7 +23,5 @@ public class ForEx02_02 {
 		System.out.println("1부터 100 까지의 합계: " + sum);
 		System.out.println("1부터 100 까지의 홀수의 합계: " + odd);
 		System.out.println("1부터 100 까지의 짝수의 합계: " + even);
-
 	}
-
 }
