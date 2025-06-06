0af6b5a11d849c04030db80471f2ccc9218164aa
bobman <wwaq1@naver.com>
2025-06-07 00:57:48 +0900

    Update IfEx01_if문기초.java

--- 20250520- 테스트,if/if문/IfEx01_if문기초.java
+++ 20250520- 테스트,if/if문/IfEx01_if문기초.java
@@ -1,7 +1,6 @@
 package com.condition;
 
-
-/*제어문의 종류
+/* 제어문의 종류
  * 1. 분기문(비교조건문) -> if, switch문
     -주어진 조건의 결과에 따라 실행 문장을 다르게 하여 전혀 다른 
     결과를 얻기 위해 사용하는 제어문
@@ -13,12 +12,14 @@ package com.condition;
    
    4. continue문: 현재 수행되는 반복 회차를 포기하고 다음 회차로 이동하는 것을 의미함 
  */
+
 public class IfEx01 {
 
 	public static void main(String[] args) {
+		
 		int a = 10;
 		
-		if(a == 10) {  //조건이 참일 때=> 조건이 맞는 문장이 하나 이상일 경우는 {}을 정해주어야함 
+		if(a == 10) {  //조건이 참일 때 => 조건이 맞는 문장이 하나 이상일 경우는 {}을 정해주어야함 
 			System.out.println("a는 10값과 같습니다.");
 		}else { //조건 거짓일 때 
 			System.out.println("a는 10값과 같지 않습니다.");
