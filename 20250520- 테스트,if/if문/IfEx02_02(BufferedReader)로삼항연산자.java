3dcadd29fa20306f673bbf9bccc1cfc5a775c5f7
bobman <wwaq1@naver.com>
2025-06-07 01:24:44 +0900

    Update IfEx02_02(BufferedReader)로삼항연산자.java

--- 20250520- 테스트,if/if문/IfEx02_02(BufferedReader)로삼항연산자.java
+++ 20250520- 테스트,if/if문/IfEx02_02(BufferedReader)로삼항연산자.java
@@ -12,13 +12,13 @@ public class IfEx02_02 {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));  // 주요 변수를 선언
 		
-		int n;  //사용자로부터 입력받은 정수를 저장할 변수
-		String str;  //홀수인지 짝수인지 대한 판결에 대한 결과를 저장하는 변수
+		int n;  // 사용자로부터 입력받은 정수를 저장할 변수
+		String str;  // 홀수인지 짝수인지 대한 판결에 대한 결과를 저장하는 변수
 		
 		System.out.print("정수 입력: ");
 		n= Integer.parseInt(br.readLine());
 		
-		str= n % 2 == 0? "짝수": "홀수";
+		str= n % 2 == 0? "짝수" : "홀수";
 		System.out.println(n + " ==> " + str);
 	}
 }
