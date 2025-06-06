298e91b64b27b51b751a837407282de21e488e25
bobman <wwaq1@naver.com>
2025-06-07 00:40:11 +0900

    Update HomeWork03_for문이용해서구구단원하는단대로출력.java

--- 과제/HomeWork03_for문이용해서구구단원하는단대로출력.java
+++ 과제/HomeWork03_for문이용해서구구단원하는단대로출력.java
@@ -11,12 +11,12 @@ public class HomeWork03 {
 		System.out.print("단 입력: ");
 		int n = sc.nextInt();
 		
-		int i, j, k;  //k는 단
+		int i, j, k;  
 		
-		for (i = 1; i < 9; i += n) {
-			for (j = 1; j <= 9; j++) {
-				for (k = 1; k <= n; k++) {
-					if (i + k <= 9)
+		for (i = 1; i < 9; i += n) { // 단 묶음의 시작
+			for (j = 1; j <= 9; j++) { // 각 단의 곱해지는 수
+				for (k = 1; k <= n; k++) { // i로부터 몇번째 단인지 
+					if (i + k <= 9)   // 9단을 넘지 않도록 제한 
 						System.out.printf("%d * %d = %d\t\t", i + k, j, ((i + k) * j));
 				}
 				System.out.println();
@@ -31,8 +31,8 @@ public class HomeWork03 {
  
  int a, b, c;
  
- for (c = 1; c <= 3; c++) {  // 총 3바퀴 돌림 
- 	for (b = 1; b <= 9; b++) { // 행 
+ for (c = 1; c <= 3; c++) {  // 묶음 
+ 	for (b = 1; b <= 9; b++) { //  행 
   		for (a = c * 3 - 1; a <= c * 3 + 1; a++) { // 열 
     				if (a < 10) System.out.printf("%2d * %2d = %2d", a, b, b * a);
  			} System.out.println(); } System.out.println(); } 
