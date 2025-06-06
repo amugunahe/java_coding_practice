eb6f227c9067dc43590e37e6a75e485a227b81ea
bobman <wwaq1@naver.com>
2025-06-07 01:22:41 +0900

    Update IfEx07_(BufferedReader)printf로 계산하기.java

--- 20250520- 테스트,if/if문/IfEx07_(BufferedReader)printf로 계산하기.java
+++ 20250520- 테스트,if/if문/IfEx07_(BufferedReader)printf로 계산하기.java
@@ -1,10 +1,13 @@
 package com.condition;
 
 import java.io.*;
+
 public class IfEx07 {
 
 	public static void main(String[] args)throws IOException {
+		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
 		int a, b; 
 		char c;
 		
@@ -13,13 +16,13 @@ public class IfEx07 {
 		
 		System.out.print("연산자 입력: ");
 		c = (char)System.in.read();
+		
 		System.in.read();
 		System.in.read();
 		System.in.skip(2);
 		
 		System.out.println("두번째 정수 입력: ");
 		b = Integer.parseInt(br.readLine());
-		
 		
 		if(c == '+') {
 			System.out.printf("%d + %d = %d", a, b, (a + b));
