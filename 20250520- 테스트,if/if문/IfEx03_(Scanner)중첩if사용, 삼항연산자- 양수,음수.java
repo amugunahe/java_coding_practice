65d5a81f60e80a5d6d302b72effe9addf5e683e2
bobman <wwaq1@naver.com>
2025-06-07 01:42:54 +0900

    Update IfEx03_(Scanner)중첩if사용, 삼항연산자- 양수,음수.java

--- 20250520- 테스트,if/if문/IfEx03_(Scanner)중첩if사용, 삼항연산자- 양수,음수.java
+++ 20250520- 테스트,if/if문/IfEx03_(Scanner)중첩if사용, 삼항연산자- 양수,음수.java
@@ -1,12 +1,13 @@
 package com.condition;
 
 /* 문] 중첩 if를 사용  
- *   사용자로부터 임의의 정수를 입력받아 입력받은 정수가 양수인지, 음수인지, 0인지를 
- *   판정하는 프로그램을 구현하시오. 
- *  
- *  단, scanner 클래스로 
- *  또한 조건 삼항연산자도구현하시오.
- */
+사용자로부터 임의의 정수를 입력받아 입력받은 정수가 양수인지, 음수인지, 0인지를 
+판정하는 프로그램을 구현하시오. 
+ 
+단, scanner 클래스로 
+또한 조건 삼항연산자도 구현하시오.
+*/
+
 import java.util.*;
 
 public class IfEx03 {
@@ -14,10 +15,12 @@ public class IfEx03 {
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);  
-						 /*int userInput;
-		                                  String str;
-		                                  System.out.print("정수 입력: ");
-		                                  userInput= sc.nextInt(); */
+		
+			 /* int userInput;
+			    String str;
+		            System.out.print("정수 입력: ");
+		            userInput= sc.nextInt(); */
+		
 		System.out.print("정수를 입력: ");
 		int userInput= sc.nextInt(); 
 		String str;
@@ -33,16 +36,18 @@ public class IfEx03 {
 			System.out.println(userInput+ "은 0입니다.");
 		}
 	}
+	
      /* 삼항 연산자
 	 str = (userInput < 0) ? "음수" : ((userInput == 0) ? "0" : "양수");
-	
-	/* 	if (userInput == 0) {
-			str = "영";
-		} else if (userInput < 0) {
-			str = "음수";
-		} else {
-			str = "양수";
-		}
-		System.out.println(userInput + "==>" + str);
+
+ 	if형
+	if (userInput == 0) {
+		str = "영";
+	} else if (userInput < 0) {
+		str = "음수";
+	} else {
+		str = "양수";
+	}
+	System.out.println(userInput + "==>" + str);
 	 */
 }
