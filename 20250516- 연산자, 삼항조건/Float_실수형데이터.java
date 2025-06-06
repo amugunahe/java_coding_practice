cafd87dd4e9c9810df1de398365ed6d9bc1bdf0b
bobman <wwaq1@naver.com>
2025-06-07 00:51:07 +0900

    Update Float_실수형데이터.java

--- 20250516- 연산자, 삼항조건/Float_실수형데이터.java
+++ 20250516- 연산자, 삼항조건/Float_실수형데이터.java
@@ -7,6 +7,7 @@ package com.test;
 public class Float {
 
 	public static void main(String[] args) {
+		
 		float var1;
 		var1 = 3.4f;
 		
@@ -16,7 +17,7 @@ public class Float {
 		System.out.println("var1: " + var1);
 		System.out.println("var2: " + var2);
 	
-	int a = 99;
-	System.out.printf("%b", (a > =100) && (a <= 200));
+		int a = 99;
+		System.out.printf("%b", (a > =100) && (a <= 200));
 	}
 }
