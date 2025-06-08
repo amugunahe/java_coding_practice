7a950894b5d2147b81e73cbd25ac998d70d13ff6
bobman <wwaq1@naver.com>
2025-06-08 13:42:07 +0900

    Update WhileEx03_반복숫자입력후0입력하고끝내고합계.java

--- 20250528- while문/While/WhileEx03_반복숫자입력후0입력하고끝내고합계.java
+++ 20250528- while문/While/WhileEx03_반복숫자입력후0입력하고끝내고합계.java
@@ -1,19 +1,22 @@
 package com.loop;
 
-/*문]
- * 사용자로부터 반복해서 숫자를 입력받다가 0을 입력하면 끝마치고
- * 합계를 출력하는 프로그램
+/*문] 사용자로부터 반복해서 숫자를 입력받다가 0을 입력하면 끝마치고
+  합계를 출력하는 프로그램
  */
+
 import java.util.*;
 
 public class WhileEx03 {
 
 	public static void main(String[] args) {
+		
 		Scanner sc = new Scanner(System.in);
 
 		int n, sum = 0;
 		boolean flag = true;
+		
 		while (flag) {
+			
 			System.out.print("정수 입력: ");
 			n = sc.nextInt();
 
