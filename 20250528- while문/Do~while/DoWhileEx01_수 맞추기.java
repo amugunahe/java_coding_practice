8eb13a93e4c018c21a0c96c3f7b2ff1bcc2862ac
bobman <wwaq1@naver.com>
2025-06-09 08:56:27 +0900

    DoWhileEx01_수 맞추기.java 업데이트

--- 20250528- while문/Do~while/DoWhileEx01_수 맞추기.java
+++ 20250528- while문/Do~while/DoWhileEx01_수 맞추기.java
@@ -1,25 +1,27 @@
 package dowhileex;
 
-/*
- * 문]
- * 난수를 이용하여 1과 100 사이의 임의의 수를 변수에 저장하고, 
- * 이 값을 맞출때까지 반복하는 프로그램을 구현
- * 
- * 사용자가 입력한 값을 변수 input, 
- * 난수는 answer 변수에 저장
- * 두 값을 비교해서 같으면 반복문을 종료
+/* 문] 난수를 이용하여 1과 100 사이의 임의의 수를 변수에 저장하고, 
+  이 값을 맞출때까지 반복하는 프로그램을 구현
+ 
+ 사용자가 입력한 값을 변수 input, 
+ 난수는 answer 변수에 저장
+ 두 값을 비교해서 같으면 반복문을 종료
  */
+
 import java.util.*;
 
 public class DoWhileEx01 {
 
 	public static void main(String[] args) {
+
 		Scanner sc = new Scanner(System.in);
 
-		int input = 0, answer = 0;
-		// 1~100 사이의 정수를 구함
+		int input = 0, answer = 0; 
+
+	// 1~100 사이의 정수를 구함
 		answer = (int) (Math.random() * 100) + 1;
 		// System.out.println(answer);
+
 		do {
 			System.out.print("1~100 사이의 정수 입력: ");
 			input = sc.nextInt();
@@ -32,5 +34,4 @@ public class DoWhileEx01 {
 		System.out.print(input + " 값과 " + answer + " 값이 같습니다.");
 		System.out.println("정답입니다!!!");
 	}
-
 }
