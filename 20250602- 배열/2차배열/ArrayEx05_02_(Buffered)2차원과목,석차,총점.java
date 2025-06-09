7a59a42d87cbf7b8df8dd7fae5bdcc40780dad28
bobman <wwaq1@naver.com>
2025-06-09 14:07:57 +0900

    Update ArrayEx05_02_(Buffered)2차원과목,석차,총점.java

--- 20250602- 배열/2차배열/ArrayEx05_02_(Buffered)2차원과목,석차,총점.java
+++ 20250602- 배열/2차배열/ArrayEx05_02_(Buffered)2차원과목,석차,총점.java
@@ -25,9 +25,11 @@ public static void main(String[] args) throws IOException {
 				sub[k][i] = Integer.parseInt(br.readLine());
 				// 각 과목별 점수 입력
 			} while (sub[k][i] < 0 || sub[k][i] > 100);
+			
 			// 입력받은 과목의 총점을 구함
 			// 총점
 			sub[k][sub[k].length - 2] += sub[k][i];
+			
 		} // end for(i)
 			// 평균:
 		avg[k] = sub[k][sub[k].length - 2] / (float) (sub[k].length - 2);
@@ -35,21 +37,19 @@ public static void main(String[] args) throws IOException {
 
 	} // end for(k)
 
-	
-	for(int k = 0; k < sub.length; k++){
-	for(int i = 0; i < sub.length; i++) {
-		if(sub[k][sub[k].length - 2]<sub[i][sub[i].length - 2]) {
+	for (int k = 0; k < sub.length; k++) {
+	for (int i = 0; i < sub.length; i++) {
+		if (sub[k][sub[k].length - 2]<sub[i][sub[i].length - 2]) {
 			sub[k][sub[k].length - 1]++;
 		}
 	}
 }
-
-	for(int k = 0; k < sub.length; k++){
+	for (int k = 0; k < sub.length; k++) {　
 		System.out.println(k+1+"번째 사람 ");
 		System.out.println("석차: " + sub[k][sub[k].length - 1] + "등");
 		System.out.println("총점: " + sub[k][sub[k].length - 2]);
 		System.out.printf("평균: %.2f%n", avg[k]);
-	}
+		}
 	
-}
+	}
 }
