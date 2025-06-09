98e5c4601d42cc086de7f3e38822e6dea3f93b9b
bobman <wwaq1@naver.com>
2025-06-09 12:29:42 +0900

    Update ArrayEx04_(Bufferered)1차원과목총점.java

--- 20250602- 배열/2차배열/ArrayEx04_(Bufferered)1차원과목총점.java
+++ 20250602- 배열/2차배열/ArrayEx04_(Bufferered)1차원과목총점.java
@@ -1,8 +1,6 @@
 package com.arraytwo;
 
-/*
- * 문] 과목별 점수를 입력받아 총점과 평균
- * 
+/* 문] 과목별 점수를 입력받아 총점과 평균
  */
 
 import java.io.*;
@@ -15,23 +13,23 @@ public class ArrayEx04 {
 
 		String[] subname = { "국어점수", "영어점수", "수학점수" };
 
-		int sub[] = new int[subname.length + 1];
-		// sub[0]: 국어점수, sub[1]: 영어점수, sub[2]:수학점수, sub[3]:총점
+		int sub[] = new int[subname.length + 1];  // sub[0]: 국어점수, sub[1]: 영어점수, sub[2]:수학점수, sub[3]:총점
 		float avg = 0.0f;
 
 		for (int i = 0; i < sub.length - 1; i++) {
 			do {
 				System.out.print(subname[i] + ":");
-				sub[i] = Integer.parseInt(br.readLine());
+				
 				// 각 과목별 점수 입력
+				sub[i] = Integer.parseInt(br.readLine()); 
 			} while (sub[i] < 0 || sub[i] > 100);
+			
 			// 입력받은 과목의 총점을 구함
-			sub[sub.length - 1] += sub[i]; // sub[3]
+			sub[sub.length - 1] += sub[i];   // sub[3]
 		}
 		avg = sub[sub.length - 1] / (float) (sub.length - 1);
 
 		System.out.println(sub[sub.length - 1]);
 		System.out.printf("평균: %.2f", avg);
 	}
-
 }
