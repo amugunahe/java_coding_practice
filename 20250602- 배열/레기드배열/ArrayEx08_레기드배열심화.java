ca932ffc6c73adba3eb1004d120e4f35677772d5
bobman <wwaq1@naver.com>
2025-06-09 19:31:53 +0900

    Update ArrayEx08_레기드배열심화.java

--- 20250602- 배열/레기드배열/ArrayEx08_레기드배열심화.java
+++ 20250602- 배열/레기드배열/ArrayEx08_레기드배열심화.java
@@ -1,12 +1,12 @@
 package com.arraytwo;
 
-/* 가변배열
+/* 가변 배열
 선언:자료형[][] 배열명; 또는 자료형[] 배열명[]; 또는 자료형 배열명[][];
 초기화: 배열명 = new 자료형[행][]
-int[][] arr= new int[3][];
-arr[0]= new int[2];
-arr[1]= new int[1];
-arr[2]= new int[3];
+int[][] arr = new int[3][];
+arr[0] = new int[2];
+arr[1] = new int[1];
+arr[2] = new int[3];
  */
 
 public class ArrayEx08 {
@@ -16,9 +16,9 @@ public class ArrayEx08 {
 		// 레기드 배열선언	
 		int[][] arr = new int[3][];
 
-		arr[0] = new int[2]; // 1행 초기화
-		arr[1] = new int[1]; // 2행 초기화
-		arr[2] = new int[3]; // 3행 초기화
+		arr[0] = new int[2]; // 1행 길이 2로 초기화
+		arr[1] = new int[1]; // 2행 길이 1로 초기화
+		arr[2] = new int[3]; // 3행 길이 3으로 초기화
 
 		arr[0][0] = 100;
 		arr[0][1] = 200;
@@ -30,10 +30,10 @@ public class ArrayEx08 {
 		arr[2][0] = 400;
 		arr[2][1] = 500;
 		arr[2][2] = 600;
-		// 3행끝
+		// 3행 끝
 
-		for (int i = 0; i < arr.length; i++) {
-			for (int j = 0; j < arr[i].length; j++) {
+		for (int i = 0; i < arr.length; i++) { // 각 행 순서대로 
+			for (int j = 0; j < arr[i].length; j++) { // 현재 행의 길이만큼 열 방문 
 				System.out.print("arr[" + i + "][" + j + "]: " + arr[i][j] + "\t");
 			}
 			System.out.println();
