828cca9057e47e4c43b14c97b7d785406185b668
bobman <wwaq1@naver.com>
2025-06-09 11:20:55 +0900

    Update Exam02_(Scanner)If두자리같은지.java

--- 20250522- Switch/Test/Exam02_(Scanner)If두자리같은지.java
+++ 20250522- Switch/Test/Exam02_(Scanner)If두자리같은지.java
@@ -27,26 +27,26 @@ public class EXam02 {
 		System.out.print("10자리 정수를 입력해라: ");
 		a = sc.nextInt();
 		
-		if(a > 9 && a <= 99) {
-			if(a / 10 == a % 10)
+		if (a > 9 && a <= 99) {
+			if (a / 10 == a % 10)
 				System.out.println("10자리와 1의 자리가 같습니다.");
 			else
 				System.out.println("10의 자리와 1의 자리가 다릅니다.");
-		}	
-		else
+		} else
 			System.out.println("범위를 벗어났습니다.");			
 		}
 	}
 
-/* 다른 답) 
+		/* 다른 답) 
 		Scanner sc = new Scanner(System.in);
+  
 		int n, ten, one;
 		
 		System.out.print("2자리 정수를 입력(10 ~ 99): ");
 		n = sc.nextInt();
 		
-		//범위
-		if(n < 10 || n > 99){
+		// 범위
+		if (n < 10 || n > 99){
 		System.out.Pritnln("정수의 범위는 10 ~ 99만 입력하세요.");
 		return;
 		}
@@ -54,9 +54,9 @@ public class EXam02 {
 		ten = n / 10;   // 10의 자리
 		one = n % 10;  // 1의 자리
 	
-	// 십의 자리와 일의 자리가 같은지를 비교
-	  if(ten == one)
+	  // 십의 자리와 일의 자리가 같은지를 비교
+	  if (ten == one)
 	  System.out.Pritnln("10자리와 1의 자리가 같습니다.");
 	  else
 	  System.out.Pritnln("10의 자리와 1의 자리가 다릅니다.");
- */
+ 		*/
