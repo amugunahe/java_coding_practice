8f6d58a4fd29c80a233d34771bc16833922bb391
bobman <wwaq1@naver.com>
2025-06-09 12:23:16 +0900

    Update ArrayEx03_알파벳대문자출력.java

--- 20250602- 배열/1차배열/ArrayEx03_알파벳대문자출력.java
+++ 20250602- 배열/1차배열/ArrayEx03_알파벳대문자출력.java
@@ -1,31 +1,24 @@
 package com.arrayex;
 
-/*
- * 문]
- * char 자료형의 배열에 알파벳 대문자를 저장하고
- * 저장된 배열의 전체요소를 출력하는 프로그램을 작성하시오.
- * 
- * ABCD~Z
- * 
- * 
- */
+/* 문] char 자료형의 배열에 알파벳 대문자를 저장하고
+저장된 배열의 전체요소를 출력하는 프로그램을 작성하시오.
+ 
+ABCD~Z
+*/
 
 public class ArrayEx03 {
 
 	public static void main(String[] args) {
-		/*
-		 * 첫번째
-		 * 
-		 * char [] arr1= new char [26]; arr1[0]= 'A'; arr1[25]= 'Z';
-		 *
-		 * 두번째
-		 * 
-		 * char[]arr2= {'A','B','C','D','E','F','G'~~'Z'};
-		 * 
-		 * 세번째
-		 * 
-		 * char[] arr3= new char[26]; for(int i=0, b=65; i<arr3.length; i++, b++) {
-		 * arr3[i]= (char)b; System.out.print(arr3[i]+" ");
+		
+		/* 첫번째 
+		 char [] arr1 = new char [26]; arr1[0] = 'A'; arr1[25] = 'Z';
+	
+		 두번째
+		char[]arr2 = {'A','B','C','D','E','F','G'~~'Z'};
+	
+		 세번째
+ 		char[] arr3 = new char[26]; for(int i = 0, b = 65; i < arr3.length; i++, b++) {
+		arr3[i] = (char)b; System.out.print(arr3[i] + " ");
 		 */
 
 		char[] arr3 = new char[26];
@@ -33,6 +26,5 @@ public class ArrayEx03 {
 			arr3[i] = (char) ('A' + i);
 			System.out.print(arr3[i] + " ");
 		}
-
 	}
 }
