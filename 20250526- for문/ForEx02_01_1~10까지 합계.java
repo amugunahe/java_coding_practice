a4b0d0346623da19a53c6092f87068ee5e8276c9
bobman <wwaq1@naver.com>
2025-06-09 11:28:06 +0900

    Update ForEx02_01_1~10까지 합계.java

--- 20250526- for문/ForEx02_01_1~10까지 합계.java
+++ 20250526- for문/ForEx02_01_1~10까지 합계.java
@@ -1,16 +1,15 @@
 package com.loop;
 
-/*
- * 문] 1~10까지의 합계를 구하는 프로그램을 구현하시오.
- * 
+/* 문] 1~10까지의 합계를 구하는 프로그램을 구현하시오.
  */
 
 public class ForEx02_01 {
 
 	public static void main(String[] args) {
+		
 		int i, sum = 0;
 		
-		for(i = 1; i <= 10; i++) {
+		for (i = 1; i <= 10; i++) {
 			sum += i;
 			System.out.println("1부터 " + i + "까지의 합계: " + sum);
 		}
