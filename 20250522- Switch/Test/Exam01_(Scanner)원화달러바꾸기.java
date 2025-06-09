5dd5ac0c23f38edd7457dbeb2300a18e01b20bdb
bobman <wwaq1@naver.com>
2025-06-09 11:19:45 +0900

    Update Exam01_(Scanner)원화달러바꾸기.java

--- 20250522- Switch/Test/Exam01_(Scanner)원화달러바꾸기.java
+++ 20250522- Switch/Test/Exam01_(Scanner)원화달러바꾸기.java
@@ -1,39 +1,38 @@
 package com.exam;
 
-/*
- * 원화를 입력받아 달러로 바꾸는 프로그램을 구현하시오.
- * 1$ = 1400
- * 
- * 원화 입력: 2800
- * 2800원은 $2.0입니다
- * 
- * 2800/1400.0
- */
+/* 원화를 입력받아 달러로 바꾸는 프로그램을 구현하시오.
+1$ = 1400
 
+원화 입력: 2800
+2800원은 $2.0입니다
+
+2800/1400.0 
+*/
 
 import java.util.*;
+
 public class Exam01 {
 	
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int won;   //원화를 입력받을 변수
 		
+		Scanner sc = new Scanner(System.in);
+		
+		int won;   // 원화를 입력받을 변수
 		final double rate = 1400.0;  // 1$ = 1400
-		double dollar = 0;  //달러로 변환하여 저장할 변수
+		double dollar = 0;  // 달러로 변환하여 저장할 변수
 		
 		System.out.print("원화 입력(숫자만 기입): ");
 		won = sc.nextInt();
 		
 		System.out.printf("%d원은 $ %.2f 입니다", won, (float)won / rate);	
 	}
-
 }
 
-		/*Scanner sc = new Scanner(System.in);
-		int won;   //원화를 입력받을 변수
-		
+		/* Scanner sc = new Scanner(System.in);
+			
+   		int won;   // 원화를 입력받을 변수
 		final double rate = 1400.0;  // 1$ = 1400
-		double dollar = 0;  //달러로 변환하여 저장할 변수
+		double dollar = 0;  // 달러로 변환하여 저장할 변수
 		
 		System.out.print("원화 입력(숫자만 기입): ");
 		won = sc.nextInt();
@@ -41,6 +40,4 @@ public class Exam01 {
 		dollar = won / rate;
 		
 		System.out.prinln(won + "은 $" + dollar + "입니다");	
-		
- * 
- */
+		 */
