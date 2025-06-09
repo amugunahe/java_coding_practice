d0c1d4a6cffa581d91baada5b7ef4f29484ea745
bobman <wwaq1@naver.com>
2025-06-09 12:27:11 +0900

    Update ArrayEx02_배열 초기화.java

--- 20250602- 배열/2차배열/ArrayEx02_배열 초기화.java
+++ 20250602- 배열/2차배열/ArrayEx02_배열 초기화.java
@@ -7,11 +7,15 @@ public class ArrayEx02 {
 		int[][] score = { // 배열의 초기화
 				{ 100, 100, 100 }, { 20, 20, 20 }, { 30, 30, 30 }, { 40, 40, 40 }, { 50, 50, 50 } };
 
-		/*
-		 * for (int i = 0; i < score.length; i++) { for (int j = 0; j < score[i].length;
-		 * j++) { System.out.print(score[i][j] + "\t"); } System.out.println(); }
+		/* for (int i = 0; i < score.length; i++) { 
+  		   for (int j = 0; j < score[i].length; j++) { 
+    			System.out.print(score[i][j] + "\t"); } 
+       			System.out.println(); 
+	  		}
 		 */
+		
 		int sum = 0;
+		
 		for (int[] temp : score) {
 			for (int i : temp) {
 				System.out.print(i + "\t");
@@ -19,10 +23,8 @@ public class ArrayEx02 {
 			System.out.println();
 		}
 		System.out.println();
-		for (int[] temp : score) {
-			// score의 각 요소(1차원 배열 주소)를 1차원 배열의 temp에 저장
-			for (int i : temp) {
-				// temp는 1차원 배열을 가리키는 참조변수
+		for (int[] temp : score) { // score의 각 요소(1차원 배열 주소)를 1차원 배열의 temp에 저장
+			for (int i : temp) { // temp는 1차원 배열을 가리키는 참조변수
 				sum += i;
 			}
 		}
