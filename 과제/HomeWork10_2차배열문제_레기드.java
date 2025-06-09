9638a09dd3604e7829ba81818282b4cda51ac0c2
bobman <wwaq1@naver.com>
2025-06-10 00:10:03 +0900

    Update HomeWork10_2차배열문제_레기드.java

--- 과제/HomeWork10_2차배열문제_레기드.java
+++ 과제/HomeWork10_2차배열문제_레기드.java
@@ -11,13 +11,12 @@ public class HomeWork10 {
 
 	public static void main(String[] args) {
 		
-	
 		int[][] arr = new int[4][];
 		
 		int n = 1;
 		
 	for (int i = 0; i < arr.length; i++) {
-		arr [i] = new int[i + 1];
+		arr [i] = new int[i + 1];   // 열의 길이가 점점 늘어남 
 		for (int j = 0; j < arr[i].length; j++) {
 			arr[i][j] = n++;
 			}
@@ -32,8 +31,7 @@ public class HomeWork10 {
 		}
 	}
 }	
-				
-		
+						
 /* 나의 답]	
 
 int num = 1;
