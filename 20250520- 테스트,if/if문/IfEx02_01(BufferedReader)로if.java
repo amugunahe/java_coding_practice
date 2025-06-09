9bf99f214e436bcf59117611ed803ca8ca3b148c
bobman <wwaq1@naver.com>
2025-06-09 11:11:50 +0900

    Update IfEx02_01(BufferedReader)로if.java

--- 20250520- 테스트,if/if문/IfEx02_01(BufferedReader)로if.java
+++ 20250520- 테스트,if/if문/IfEx02_01(BufferedReader)로if.java
@@ -23,7 +23,7 @@ public class IfEx02_01 {
 		n= Integer.parseInt(br.readLine());
 		
 		
-		if(n % 2 == 0) { //홀수인지 짝수인지를 판정
+		if (n % 2 == 0) { //홀수인지 짝수인지를 판정
 			str= "짝수"; 
 		} else {
 			str= "홀수";
