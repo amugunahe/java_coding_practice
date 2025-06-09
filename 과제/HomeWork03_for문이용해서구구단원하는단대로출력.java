be621e46689461ad06a70b3b2a4b8d00fb96770b
bobman <wwaq1@naver.com>
2025-06-09 20:40:20 +0900

    Update HomeWork03_for문이용해서구구단원하는단대로출력.java

--- 과제/HomeWork03_for문이용해서구구단원하는단대로출력.java
+++ 과제/HomeWork03_for문이용해서구구단원하는단대로출력.java
@@ -15,7 +15,7 @@ public class HomeWork03 {
 		
 		for (i = 1; i < 9; i += n) { // 단 묶음의 시작
 			for (j = 1; j <= 9; j++) { // 각 단의 곱해지는 수
-				for (k = 1; k <= n; k++) { // i로부터 몇번째 단인지 
+				for (k = 1; k <= n; k++) { // 묶음 내 단 번호 
 					if (i + k <= 9)   // 9단을 넘지 않도록 제한 
 						System.out.printf("%d * %d = %d\t\t", i + k, j, ((i + k) * j));
 				}
@@ -26,18 +26,21 @@ public class HomeWork03 {
 	}
 }
 
-/* Scanner sc = new Scanner(System.in); 
- 
- int a, b, c;
- 
- for (c = 1; c <= 3; c++) {  // 묶음 
- 	for (b = 1; b <= 9; b++) { //  곱해지는 수 (행)
-  		for (a = c * 3 - 1; a <= c * 3 + 1; a++) { // 단 (열) 
-    				if (a < 10) 
-				System.out.printf("%2d * %2d = %2d", a, b, b * a);
- 				} System.out.println(); 
- 		} System.out.println(); 
-	} 
-   } 
+/*  내가 쓴 답]
+        int a, b, c;
+
+        for (c = 1; c <= 3; c++) { // 묶음 (1 ~ 3)
+            for (b = 1; b <= 9; b++) { // 곱해지는 수
+                for (a = c * 3 - 1; a <= c * 3 + 1; a++) { // 단
+                    if (a <= 9) {
+                        System.out.printf("%2d * %2d = %2d\t", a, b, a * b);
+                    }
+                }
+                System.out.println(); // 한 줄 끝날 때마다 줄바꿈
+            }
+            System.out.println(); // 묶음 끝나고 한 줄 띄우기
+        }
+    }
 }
+
 */
