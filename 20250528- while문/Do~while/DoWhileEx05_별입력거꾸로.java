7c5627b3ab561f6b4792ffb1db52ec92979ef0f8
bobman <wwaq1@naver.com>
2025-06-09 09:29:14 +0900

    Update DoWhileEx05_별입력거꾸로.java

--- 20250528- while문/Do~while/DoWhileEx05_별입력거꾸로.java
+++ 20250528- while문/Do~while/DoWhileEx05_별입력거꾸로.java
@@ -1,20 +1,20 @@
 package dowhileex;
 
-/*문]
-  do~while문을 이용하여 아래와 같이 출력하도록 프로그램을 구현하시오.
+/* 문] do~while문을 이용하여 아래와 같이 출력하도록 프로그램을 구현하시오.
   
-   *****
-   ****
-   ***
-   **
-   * 
- */
+*****
+****
+***
+**
+* 
+*/
 
 public class DoWhileEx05 {
 
 	public static void main(String[] args) {
 
 		int i = 5;
+		
 		do {
 			int j = 0;
 			do {
