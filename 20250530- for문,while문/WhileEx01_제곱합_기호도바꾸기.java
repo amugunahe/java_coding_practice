7202a4285461c3610945d46b8052972930815eb0
bobman <wwaq1@naver.com>
2025-06-09 10:30:02 +0900

    Update WhileEx01_제곱합_기호도바꾸기.java

--- 20250530- for문,while문/WhileEx01_제곱합_기호도바꾸기.java
+++ 20250530- for문,while문/WhileEx01_제곱합_기호도바꾸기.java
@@ -1,36 +1,32 @@
 package com.exam;
 
-/*
- * 문]
- * -1^2+2^2-3^2+4^2-5^2+6^2~~~~~~99^2의 합계를 구하는 //제곱
- * 프로그램을 구현하시오.
- * 단, while문 사용
- * 
- * 결과: 5050
- * 
+/* 문] -1^2+2^2-3^2+4^2-5^2+6^2~~~~~~99^2의 합계를 구하는 제곱 프로그램을 구현하시오.
+ 단, while문 사용
+
+결과: 5050 
  */
+
 public class WhileEx01 {
 
 	public static void main(String[] args) {
 
 		int i = 0;
 		int sum = 0;
-		int plus=1;
+		int plus = 1;
 
-		/*while (i <= 100) {
+		/* while (i <= 100) {
 			if (i % 2 == 0) {
 				sum += i * i;
 			} else {
 				sum += -(i * i);
-			}*/
+			}
+   		*/
 		
-		 while (i<=100){
-			 
+		 while (i <= 100){
 			plus= -plus;
-			sum+= plus*(i*i);
+			sum += plus * (i * i);
 			i++;
 		}
 		System.out.println("합계: " + sum);
 	}
 }
-
