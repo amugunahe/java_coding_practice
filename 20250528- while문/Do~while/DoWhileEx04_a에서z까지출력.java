7389a6800765fc8509022d471ae73647b7386906
bobman <wwaq1@naver.com>
2025-06-09 09:28:07 +0900

    Update DoWhileEx04_a에서z까지출력.java

--- 20250528- while문/Do~while/DoWhileEx04_a에서z까지출력.java
+++ 20250528- while문/Do~while/DoWhileEx04_a에서z까지출력.java
@@ -1,19 +1,17 @@
 package dowhileex;
 
-/*
- * a~z까지 출력
- */
+/* a~z까지 출력
+*/
 
 public class DoWhileEx04 {
 
 	public static void main(String[] args) {
 
 		char c = 'a';
+		
 		do {
 			System.out.print(c + " ");
 			c = (char) (c + 1);
 		} while (c <= 'z');
-
 	}
-
 }
