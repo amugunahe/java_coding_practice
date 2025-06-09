a7af8377e316a9d13befb654c22a8ecc99a08e35
bobman <wwaq1@naver.com>
2025-06-09 19:09:50 +0900

    Update ArrayEx05_02_(Buffered)2차원과목,석차,총점.java

--- 20250602- 배열/2차배열/ArrayEx05_02_(Buffered)2차원과목,석차,총점.java
+++ 20250602- 배열/2차배열/ArrayEx05_02_(Buffered)2차원과목,석차,총점.java
@@ -12,7 +12,7 @@ public static void main(String[] args) throws IOException {
 
 	int sub[][] = new int[3][subname.length + 2];
 	// 0: 첫번째 사람, 1: 두번째 사람, 2: 세번째 사람
-	// sub[0]: 국어점수, sub[1]: 영어점수, sub[2]:수학점수, sub[3]:총점, sub[4]:석차
+	// sub[0]: 국어점수, sub[1]: 영어점수, sub[2]: 수학점수, sub[3]: 총점, sub[4]: 석차
 	
 	float[] avg = new float[3];
 
@@ -26,30 +26,32 @@ public static void main(String[] args) throws IOException {
 				// 각 과목별 점수 입력
 			} while (sub[k][i] < 0 || sub[k][i] > 100);
 			
-			// 입력받은 과목의 총점을 구함
-			// 총점
+			// 총점 계산 (입력받은 과목의 총점을 구함)
 			sub[k][sub[k].length - 2] += sub[k][i];
 			
-		} // end for(i)
-			// 평균:
+		} // end for(i)- 안쪽 for문 끝 
+		
+		 // 평균 계산
 		avg[k] = sub[k][sub[k].length - 2] / (float) (sub[k].length - 2);
+
+		// 석차를 1등으로 초기화
 		sub[k][sub[k].length - 1] = 1;
 
-	} // end for(k)
-
+	} // end for(k)- 바깥쪽 for문 끝
+	
+	// 석차 계산
 	for (int k = 0; k < sub.length; k++) {
 	for (int i = 0; i < sub.length; i++) {
-		if (sub[k][sub[k].length - 2]<sub[i][sub[i].length - 2]) {
+		if (sub[k][sub[k].length - 2] < sub[i][sub[i].length - 2]) {
 			sub[k][sub[k].length - 1]++;
 		}
 	}
 }
 	for (int k = 0; k < sub.length; k++) {　
-		System.out.println(k+1+"번째 사람 ");
+		System.out.println(k + 1 + "번째 사람 ");
 		System.out.println("석차: " + sub[k][sub[k].length - 1] + "등");
 		System.out.println("총점: " + sub[k][sub[k].length - 2]);
 		System.out.printf("평균: %.2f%n", avg[k]);
 		}
-	
 	}
 }
