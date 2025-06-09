61147d61588b3045b0149638919ac19ffc38168d
bobman <wwaq1@naver.com>
2025-06-09 09:26:34 +0900

    Update DoWhileEx02_369짝짝.java

--- 20250528- while문/Do~while/DoWhileEx02_369짝짝.java
+++ 20250528- while문/Do~while/DoWhileEx02_369짝짝.java
@@ -3,22 +3,19 @@ package dowhileex;
 public class DoWhileEx02 {
 
 	public static void main(String[] args) {
+		
 		for (int i = 1; i <= 100; i++) {
-			System.out.printf("i=%d ", i);
+			System.out.printf("i = %d ", i);
 
 			int temp = i;
 			do {
-
 				if (temp % 10 % 3 == 0 && temp % 10 != 0) {
-					// temp%10이 3의 배수인지 확인(0은 제외)
-					// 97 temp%10=7, temp%3=1,
+					// temp % 10이 3의 배수인지 확인(0은 제외)
+					// 97 temp % 10 = 7, temp % 3 = 1,
 					System.out.print("짝");
 				}
 			} while ((temp /= 10) != 0);
 			System.out.println();
-
 		}
-
 	}
-
 }
