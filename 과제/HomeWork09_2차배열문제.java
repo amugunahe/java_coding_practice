68904fa409341b14f619b563e6e3fcedf5fed766
bobman <wwaq1@naver.com>
2025-06-10 00:08:29 +0900

    Update HomeWork09_2차배열문제_1부터 4씩 증가하는 배열.java

--- 과제/HomeWork09_2차배열문제.java
+++ 과제/HomeWork09_2차배열문제.java
@@ -12,24 +12,24 @@ public class HomeWork09 {
 
 	public static void main(String[] args) {
 		
-		int cnt_i = 4, cnt_j = 5;
+		int cnt_i = 4, cnt_j = 5;   // 4행 5열
 		
 		int[][] arr = new int[cnt_i][cnt_j];
 		
 		// 행 값 저장 
 		for (int i = 0; i < arr.length; i++) {
-			for (int j = 0; j < arr.length; j++) {
+			for (int j = 0; j < arr[i].length; j++) {
 				
 				/* 1. 숫자는 하나씩 증가한다.
 				   2. 행의 시작은 행 * 열 - i, 열의 시작은 행의 크기 * 열의 인덱스 
 				*/
-				arr[i][j] = (i + 1) + (cnt_j * j);			
+				arr[i][j] = (i + 1) + (cnt_j * j);  // 각 행의 시작 값은 i + 1
 				}
 		}
 		
 		// 배열의 저장된 값 출력 
 		for (int i = 0; i < arr.length; i++) {
-			for(int j = 0; j <arr[i].length; j++) {
+			for(int j = 0; j < arr[i].length; j++) {
 				System.out.print(arr[i][j] + "\t");
 			}
 			System.out.println();
