e1edca461c5151f2c374cafc3ba09be40c8b2753
bobman <wwaq1@naver.com>
2025-06-09 11:29:11 +0900

    Update ForEx04_삼각형출력하기.java

--- 20250526- for문/ForEx04_삼각형출력하기.java
+++ 20250526- for문/ForEx04_삼각형출력하기.java
@@ -20,8 +20,8 @@ System.out.println("**********");
 		System.out.print("*을 입력할 라인 수 입력:");
 		int line = sc.nextInt();
 		
-		for(int j = 1; j <= line; j++) {  // 행
-		for(int i = 1; i <= j; i++) { // 열
+		for (int j = 1; j <= line; j++) {  // 행
+		for (int i = 1; i <= j; i++) { // 열
 			System.out.print("*");				
 			}
 			System.out.println();
