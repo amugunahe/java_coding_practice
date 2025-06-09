abd8a8edf60128a47e894554c28fa9c96f7345df
bobman <wwaq1@naver.com>
2025-06-09 09:27:30 +0900

    Update DoWhileEx03_1에서10덧셈합계.java

--- 20250528- while문/Do~while/DoWhileEx03_1에서10덧셈합계.java
+++ 20250528- while문/Do~while/DoWhileEx03_1에서10덧셈합계.java
@@ -1,30 +1,24 @@
 package dowhileex;
 
-/* 문]
- * do ~while문을 이용하여 1에서 10까지 덧셈으로 표시하고 합계를 구하는 
- * 프로그램을 구현하시오. 
- * 
- * 결과 
- * 1+2+3+4+5+6+7+8+9+10==55
- * 
- * 
- */
+/* 문] do ~while문을 이용하여 1에서 10까지 덧셈으로 표시하고 합계를 구하는 
+ 프로그램을 구현하시오. 
+ 
+결과 
+1+2+3+4+5+6+7+8+9+10 == 55 
+*/
 
 
 public class DoWhileEx03 {
 
 	public static void main(String[] args) {
 		
-		int sum=0;;
-		int i =1;
-		do {
-			sum+=i;
-			i++;
-		}while(i<=10);
+		int sum = 0;
+		int i = 1;
 		
+		do {
+			sum += i;
+			i++;
+		} while(i <= 10);
 		System.out.println(sum);
- 
-
 	}
-
 }
