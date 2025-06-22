e326cda80381e595669598438700b6c93af55e0e
bobman <wwaq1@naver.com>
2025-06-22 15:36:47 +0900

    Update BooleanEx_불린.java

--- 20250515- 변수, 상수/BooleanEx_불린.java
+++ 20250515- 변수, 상수/BooleanEx_불린.java
@@ -1,14 +1,14 @@
-package com.variable;
+package com.variable; // 이 코드가 속한 폴더(패키지)를 나타내요.
 
 /* boolean: 자바에서 유일한 논리 자료형: 크기 1byte이지만 
 실제 사용은 1bit만 사용함
-*/
+*/ // 여기에 이 코드에 대한 설명이 적혀있어요.
 
-public class BooleanEx {
+public class BooleanEx { // 'BooleanEx'라는 이름의 프로그램을 시작해요.
 
-	public static void main(String[] args) {
+	public static void main(String[] args) { // 프로그램이 실제로 시작되는 부분이에요.
 		
-		boolean b = true;
-		System.out.println("변수 b의 값은: " + b);
+		boolean b = true; // 'b'라는 이름의 'boolean' 타입 변수를 만들고 'true' 값을 넣어줘요.
+		System.out.println("변수 b의 값은: " + b); // 'b' 변수에 저장된 값을 화면에 보여줘요.
 	}
 }
