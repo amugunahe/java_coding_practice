4e89e47fcf15ef68494882ebcd897dca37383a1f
bobman <wwaq1@naver.com>
2025-06-24 14:20:04 +0900

    Update Car.java

--- 20250619-상속/상속- 다형성/앰뷸런스/Car.java
+++ 20250619-상속/상속- 다형성/앰뷸런스/Car.java
@@ -1,15 +1,27 @@
-package com.polymorphism;
+package com.polymorphism; // 이 클래스가 속한 패키지다.
+
+/*
+  Car 클래스: 모든 차들의 '부모' 클래스.
+ 
+ *핵심:
+ - 모든 차들이 공통으로 가질 속성 (색깔, 문 개수)이랑
+  - 공통으로 할 수 있는 행동 (운전, 멈추기)을 정의한다.
+  - 이게 다형성에서 '업캐스팅'의 기반이 된다. (자식 객체를 부모 타입으로 다룰 수 있게 해줌)
+ */
 
 public class Car {
 
-	String color;
-	int dorr;
-	
+	String color; // 차의 색깔 (속성)
+	int door;     // 차의 문 개수 (속성)
+
+	// 차를 운전하는 행동 (메소드)
 	void drive() {
 		System.out.println("drive,~~~~~");
 	}
 
+	// 차를 멈추는 행동 (메소드)
 	void stop(){
 		System.out.println("hoho");
 	}
 }
+
