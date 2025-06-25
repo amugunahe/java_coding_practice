f422b6818e940de80b5daabe9a67b1739aca87ab
bobman <wwaq1@naver.com>
2025-06-26 06:43:54 +0900

    Update ForEx05_[]대각선출력.java

--- 20250526- for문/ForEx05_[]대각선출력.java
+++ 20250526- for문/ForEx05_[]대각선출력.java
@@ -4,15 +4,18 @@ public class ForEx05 {
 
 	public static void main(String[] args) {
 		
-		for (int i = 1; i <= 9; i++) { // 행
-			for (int j = 1; j <= 9; j++) { // 열 컬럼
-				if (i == j) {
-					System.out.printf("[%d, %d]", i, j);
-				} else {
-				System.out.printf("%8c",' ');
+		// 바깥쪽 for문: 행(줄)을 제어한다. i는 1부터 9까지 증가.
+		for (int i = 1; i <= 9; i++) { 
+			// 안쪽 for문: 열(칸)을 제어한다. j는 1부터 9까지 증가.
+			for (int j = 1; j <= 9; j++) { 
+				// 핵심 로직: i와 j가 같을 때만 숫자를 출력한다. (대각선 조건)
+				if (i == j) { // 현재 행(i)과 열(j)의 값이 같으면
+					System.out.printf("[%d, %d]", i, j); // "[행, 열]" 형식으로 출력
+				} else { // i와 j가 다르면
+					System.out.printf("%8c",' '); // 8칸짜리 공백을 출력해서 간격 맞추기
 				}
 			}
-			System.out.println();
+			System.out.println(); // 한 줄 출력 끝나면 다음 줄로 이동
 		}
 	}
 }
