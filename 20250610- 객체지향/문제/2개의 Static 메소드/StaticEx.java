3cfd70cad69321d62d08b19589dde4c2ced27b1d
bobman <wwaq1@naver.com>
2025-06-26 00:43:02 +0900

    Update StaticEx.java

--- 20250610- 객체지향/문제/2개의 Static 메소드/StaticEx.java
+++ 20250610- 객체지향/문제/2개의 Static 메소드/StaticEx.java
@@ -1,4 +1,4 @@
-package com.functionex; // 이 클래스도 'ArrayUtil'이랑 같은 패키지(폴더)에 속한다, 씨발!
+package com.functionex; // 이 클래스도 'ArrayUtil'이랑 같은 패키지(폴더)에 속한다
                          // 패키지는 관련 클래스들을 묶어 관리하는 폴더와 같은 개념이다.
 
 /*
@@ -14,8 +14,7 @@ package com.functionex; // 이 클래스도 'ArrayUtil'이랑 같은 패키지(�
 class StaticEx { // 'StaticEx'라는 이름의 클래스다. (이 파일 하나로 실행한다!)
 
     public static void main(String[] args) { // 이 메소드는 자바 프로그램이 시작될 때 가장 먼저 실행되는 곳이다.
-                                             // 프로그램의 '입구'라고 생각하면 된다, 씨발!
-
+                                             // 프로그램의 '입구'라고 생각하면 된다
         // 1. 첫 번째 정수형 배열 'array1'을 만든다.
         // {1, 5, 7, 9}라는 숫자들을 담고 있다.
         int[] array1 = { 1, 5, 7, 9 };
@@ -27,7 +26,7 @@ class StaticEx { // 'StaticEx'라는 이름의 클래스다. (이 파일 하나�
         // 3. 이제 'ArrayUtil' 클래스의 'concat' 메소드를 사용한다! (존나 중요!)
         // 'ArrayUtil.concat(array1, array2)'는 'array1'과 'array2'를 합쳐서 새로운 배열을 만들어준다.
         // 그리고 그 새롭게 합쳐진 배열을 'array3'이라는 변수에 저장한다.
-        // 'array3' 안에는 {1, 5, 7, 9, 3, 6, -1, 100, 7} 이렇게 순서대로 숫자 존나게 들어가 있을 거다, 씨발!
+        // 'array3' 안에는 {1, 5, 7, 9, 3, 6, -1, 100, 7} 이렇게 순서대로 숫자 존나게 들어가 있을 거다
         int[] array3 = ArrayUtil.concat(array1, array2);
 
         // 4. 다음으로, 'ArrayUtil' 클래스의 'print' 메소드를 사용해서 'array3'의 내용을 출력한다!
